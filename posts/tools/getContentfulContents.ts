1b87ce2855a30ff8823b9460ea2f88cbab629b62
Shota Kubota <ta2@o2p.jp>
2019-08-05 21:06:42 +0900

    chore(tools): add contentful contents getting tool

---
+++ posts/tools/getContentfulContents.ts
@@ -0,0 +1,40 @@
+import { join as pathJoin } from 'path';
+import { writeFile as legacyWriteFile } from 'fs';
+import { promisify } from 'util';
+import { Nullable } from 'option-t/lib/Nullable/Nullable';
+
+import { EntryValue } from '../entry/entryValue';
+import { entryGateway } from '../entry/entryContext';
+
+const writeFile = promisify(legacyWriteFile);
+
+const BASE_DIR = pathJoin(__dirname, '..');
+const DIST_DIR = 'data';
+const OUTPUT_FILE = 'entries.json';
+const DESTINATION_FILE = `${BASE_DIR}/${DIST_DIR}/${OUTPUT_FILE}`;
+
+function progressMessage(message: string) {
+  // tslint:disable-next-line: no-console
+  console.log(message);
+}
+
+export async function getContentfulContents() {
+  let entries: Nullable<ReadonlyArray<EntryValue>> = null;
+
+  progressMessage('> Starting contents fetch');
+
+  try {
+    entries = await entryGateway.fetchAllEntries();
+  } catch (err) {
+    throw new Error(err);
+  }
+
+  progressMessage('> Fetched contents');
+
+  const json = JSON.stringify(entries);
+  await writeFile(DESTINATION_FILE, json);
+
+  progressMessage(`> Contents written to ${DESTINATION_FILE}`);
+}
+
+getContentfulContents();
