25e2f78b3b73ff59e475801381a3577ebd648450
Shota Kubota <ta2@o2p.jp>
2019-08-06 11:07:27 +0900

    refactor(pages/top): remove TopPage component

--- posts/pages/index.tsx
+++ posts/pages/index.tsx
@@ -1,8 +1,32 @@
+import React from 'react';
+import Link from 'next/link';
+import { isNotNull } from 'option-t/lib/Nullable/Nullable';
+
 import entries from '../data/entries.json';
-import { TopPage } from './top/TopPage';
 
-const entryPoint = () => {
-  return <TopPage entries={entries} />;
-};
+const TopPage = (): JSX.Element => (
+  <React.Fragment>
+    <h2 className="p-top-articles-title">最近の記事</h2>
 
-export default entryPoint;
+    {isNotNull(entries) ? (
+      entries.map(entry => {
+        const { excerpt, id, slug, title } = entry;
+
+        return (
+          <section className="p-top-article" key={id}>
+            <h1 className="p-top-article__title">
+              <Link href="/entry/[slug]" as={`/entry/${slug}`}>
+                {title}
+              </Link>
+            </h1>
+            <p dangerouslySetInnerHTML={{ __html: excerpt }} />
+          </section>
+        );
+      })
+    ) : (
+      <p>記事はありません。</p>
+    )}
+  </React.Fragment>
+);
+
+export default TopPage;
