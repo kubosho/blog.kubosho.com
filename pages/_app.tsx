e7c64d5bfdef2c7e4746438a9ff846f33171c5a2
Shota Kubota <ta2@o2p.jp>
2019-08-14 01:15:19 +0900

    feat(app): set responsible font size

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,18 @@ import App, { Container } from 'next/app';
 import { createGlobalStyle } from 'styled-components';
 import { TEXT_COLOR } from '../common_styles/text';
 
+/*
+  viewport width:
+  - 320px => 16px
+  - 1366px => 17.3075px
+  - 1920px => 18px
+  - 3840px => 20.4px
+*/
+const BASE_FONT_SIZE = 'calc((100vw - 320px) / 800 + 1rem)';
 const GlobalStyle = createGlobalStyle`
   body {
     color: ${TEXT_COLOR};
+    font-size: ${BASE_FONT_SIZE};
   }
 `;
 
