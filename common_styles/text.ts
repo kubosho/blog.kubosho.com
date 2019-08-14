3f8dca9f35eac244d3dd05a6a0316a6be5e077da
Shota Kubota <ta2@o2p.jp>
2019-08-14 23:57:10 +0900

    feat(styles/text): add text constant value

--- common_styles/text.ts
+++ common_styles/text.ts
@@ -1,1 +1,2 @@
-export const TEXT_COLOR = '#191919';
+export const BASE_FONT_SIZE = 'calc((100vw - 320px) / 800 + 1rem)';
+export const LINE_HEIGHT = 1.8;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import App, { Container } from 'next/app';
 import { createGlobalStyle } from 'styled-components';
 import { TEXT_COLOR } from '../common_styles/color';
+import { BASE_FONT_SIZE, LINE_HEIGHT } from '../common_styles/text';
 
 /*
   viewport width:
@@ -10,11 +11,11 @@ import { TEXT_COLOR } from '../common_styles/color';
   - 1920px => 18px
   - 3840px => 20.4px
 */
-const BASE_FONT_SIZE = 'calc((100vw - 320px) / 800 + 1rem)';
 const GlobalStyle = createGlobalStyle`
   body {
     color: ${TEXT_COLOR};
     font-size: ${BASE_FONT_SIZE};
+    line-height: ${LINE_HEIGHT};
   }
 `;
 
