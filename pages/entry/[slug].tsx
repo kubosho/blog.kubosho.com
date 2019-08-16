f3a413a881ffcc83bbe9faba9cacfcd659c0054d
Shota Kubota <ta2@o2p.jp>
2019-08-17 02:26:23 +0900

    feat(entry): add blockquote sytle for entry page

--- pages/entry/[slug].tsx
+++ pages/entry/[slug].tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import { NextPageContext } from 'next';
 import Head from 'next/head';
+import { createGlobalStyle } from 'styled-components';
 
 import { EntryValue } from '../../entry/entryValue';
 import { SITE_TITLE } from '../../constants';
-import { CONTENTS_SEPARATOR_SPACE } from '../../common_styles/space';
+import { ACCENT_COLOR, MAIN_COLOR } from '../../common_styles/color';
+import { CONTENTS_SEPARATOR_SPACE, SPACE } from '../../common_styles/space';
 import { SITE_WIDTH } from '../../common_styles/size';
 import { SiteHeader } from '../../components/SiteHeader';
 import { SiteFooter } from '../../components/SiteFooter';
@@ -24,6 +26,41 @@ type Props = {
   entry: EntryValue;
 };
 
+const GlobalStyle = createGlobalStyle`
+  blockquote {
+    position: relative;
+    padding: ${SPACE} calc(1rem + ${SPACE});
+    border: ${ACCENT_COLOR} dashed;
+    border-width: 1px 0;
+  }
+
+  blockquote > p {
+    margin: 0;
+  }
+
+  blockquote::before,
+  blockquote::after {
+    position: absolute;
+    height: 0;
+    color: ${MAIN_COLOR};
+    font-size: 3rem;
+    font-weight: 900;
+    line-height: 1;
+  }
+
+  blockquote::before {
+    content: '❝';
+    top: 0.3rem;
+    right: calc(100% - 1rem);
+  }
+
+  blockquote::after {
+    content: '❞';
+    bottom: 2rem;
+    left: calc(100% - 1rem);
+  }
+`;
+
 const Entry = (props: Props): JSX.Element => {
   const { id, title, content } = props.entry;
 
@@ -34,6 +71,7 @@ const Entry = (props: Props): JSX.Element => {
           {title}: {SITE_TITLE}
         </title>
       </Head>
+      <GlobalStyle />
       <SiteHeader />
       <SiteContents>
         <Container key={id}>
