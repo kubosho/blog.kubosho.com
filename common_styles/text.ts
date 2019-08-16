c943383ce553ddfd4dd55a341d109d850f3cc613
Shota Kubota <ta2@o2p.jp>
2019-08-16 22:28:55 +0900

    chore(common_styles): move comment to text style file

--- common_styles/text.ts
+++ common_styles/text.ts
@@ -1,2 +1,9 @@
+/*
+  viewport width:
+  - 320px => 16px
+  - 1366px => 17.3075px
+  - 1920px => 18px
+  - 3840px => 20.4px
+*/
 export const BASE_FONT_SIZE = 'calc((100vw - 320px) / 800 + 1rem)';
 export const LINE_HEIGHT = 1.8;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,6 @@ import { createGlobalStyle } from 'styled-components';
 import { BACKGROUND_COLOR, TEXT_COLOR } from '../common_styles/color';
 import { BASE_FONT_SIZE, LINE_HEIGHT } from '../common_styles/text';
 
-/*
-  viewport width:
-  - 320px => 16px
-  - 1366px => 17.3075px
-  - 1920px => 18px
-  - 3840px => 20.4px
-*/
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: ${BACKGROUND_COLOR};
