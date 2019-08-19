2ae07fb1e0bf616ff7d94762bb8660e23534196f
Shota Kubota <ta2@o2p.jp>
2019-08-19 10:26:49 +0900

    feat(SiteFooter): add margin to profile image

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { CONTENTS_SEPARATOR_SPACE } from '../common_styles/space';
+import { CONTENTS_SEPARATOR_SPACE, SPACE } from '../common_styles/space';
 import { SITE_WIDTH } from '../common_styles/size';
 
 const Footer = styled.footer`
@@ -9,13 +9,16 @@ const Footer = styled.footer`
   max-width: ${SITE_WIDTH};
   margin: ${CONTENTS_SEPARATOR_SPACE} auto calc(${CONTENTS_SEPARATOR_SPACE} / 2);
 `;
+const Image = styled.img`
+  margin-right: ${SPACE};
+`;
 const Profile = styled.p`
   margin: 0;
 `;
 
 export const SiteFooter = (): JSX.Element => (
   <Footer>
-    <img src="/static/images/kubosho.jpg" alt="このブログを作った人のアイコン" width="60" height="60" />
+    <Image src="/static/images/kubosho.jpg" alt="このブログを作った人のアイコン" width="60" height="60" />
     <Profile>
       kuboshoの個人ブログです。仕事で携わっているソフトウェア開発や、ダーツやゲームなどの趣味について書きます。
     </Profile>
