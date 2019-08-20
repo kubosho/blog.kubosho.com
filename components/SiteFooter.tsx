b7be3a43d7c56ece097ed92142d7c240343f99e1
Shota Kubota <ta2@o2p.jp>
2019-08-21 02:22:39 +0900

    fix(SiteFooter): fix image not found

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -20,7 +20,7 @@ const Profile = styled.p`
 
 export const SiteFooter = (): JSX.Element => (
   <Footer>
-    <Image src="/static/images/kubosho.jpg" alt="このブログを作った人のアイコン" width="60" height="60" />
+    <Image src="/static/images/icon.png" alt="このブログを作った人のアイコン" width="60" height="60" />
     <Profile>
       kuboshoの個人ブログです。仕事で携わっているソフトウェア開発や、ダーツやゲームなどの趣味について書きます。
     </Profile>
