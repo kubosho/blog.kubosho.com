1343ebcc6bb98980ff52a12b32d2d137a5163620
Shota Kubota <ta2@o2p.jp>
2019-08-20 20:49:59 +0900

    feat(pages): add Google Analytics tracking code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,17 @@
-import Document from 'next/document';
+import Document, { Main, NextScript, Head } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import { GA_TRACKING_ID } from '../constants';
 
-export default class MyDocument extends Document {
+type Props = {
+  isProduction: boolean;
+};
+
+export default class MyDocument extends Document<Props> {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
+
+    const isProduction = process.env.NODE_ENV === 'production';
 
     try {
       ctx.renderPage = () =>
@@ -15,6 +22,7 @@ export default class MyDocument extends Document {
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
+        isProduction,
         styles: (
           <>
             {initialProps.styles}
@@ -26,4 +34,34 @@ export default class MyDocument extends Document {
       sheet.seal();
     }
   }
+
+  render() {
+    const { isProduction } = this.props;
+    return (
+      <html>
+        <Head />
+        <body>
+          {isProduction && (
+            <>
+              <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+              <script dangerouslySetInnerHTML={setGATag()} />
+            </>
+          )}
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
+
+function setGATag() {
+  return {
+    __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', ${GA_TRACKING_ID});
+      `,
+  };
 }
