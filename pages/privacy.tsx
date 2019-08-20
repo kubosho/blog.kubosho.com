2cce51b0bd96b3b23fc1afeb458f3962c30b7808
Shota Kubota <ta2@o2p.jp>
2019-08-20 10:48:46 +0900

    feat(pages): add privacy policy page

---
+++ pages/privacy.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Head from 'next/head';
+import styled from 'styled-components';
+
+import { SiteHeader } from '../components/SiteHeader';
+import { SiteFooter } from '../components/SiteFooter';
+import { CONTENTS_SEPARATOR_SPACE } from '../common_styles/space';
+import { SITE_WIDTH } from '../common_styles/size';
+import { SITE_TITLE } from '../constants';
+
+const SiteContents = styled.main`
+  max-width: ${SITE_WIDTH};
+  margin: ${CONTENTS_SEPARATOR_SPACE} auto 0;
+  padding: 0 1rem;
+`;
+
+const PrivacyPolicyPage = (): JSX.Element => (
+  <React.Fragment>
+    <Head>
+      <title>プライバシーポリシー: {SITE_TITLE}</title>
+    </Head>
+    <SiteHeader />
+    <SiteContents>
+      <p>当ブログでは内容の改善を目的として、Googleアナリティクスによるアクセス分析をおこなっています。</p>
+      <p>
+        Googleアナリティクスは、Cookie(クッキー)により、匿名のトラフィックデータを収集しています。
+        <br />
+        Cookieに含まれるデータは利用者の個人情報を特定しません。利用者はCookieを無効にした状態で当サイトにアクセスできます。
+      </p>
+      <p>
+        詳しくは
+        <a href="https://policies.google.com/technologies/partner-sites">
+          Google のサービスを使用するサイトやアプリから収集した情報の Google による使用 – ポリシーと規約 – Google
+        </a>
+        を参照してください。
+      </p>
+    </SiteContents>
+    <SiteFooter />
+  </React.Fragment>
+);
+
+export default PrivacyPolicyPage;
