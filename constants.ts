3c62783cb99bf2d0323db2255bb7f23731360bd5
Shota Kubota <ta2@o2p.jp>
2019-08-21 01:30:46 +0900

    feat(constants): add site url constant value

--- constants.ts
+++ constants.ts
@@ -1,3 +1,5 @@
 export const SITE_DESCRIPTION = 'kuboshoの個人ブログです。ソフトウェア開発や、ゲームやダーツなどについて書きます。';
 export const SITE_TITLE = "I'm kubosho";
+export const SITE_URL = 'https://blog.kubosho.com';
+
 export const GA_TRACKING_ID = 'UA-25380313-10';
