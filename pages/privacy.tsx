bd9343570fbbfb9b44749bc1c58dcd7aa59227c1
Shota Kubota <ta2@o2p.jp>
2019-08-24 13:28:13 +0900

    feat(Privacy): tweak privacy policy page

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 
 import { SiteHeader } from '../components/SiteHeader';
 import { SiteFooter } from '../components/SiteFooter';
+import { MAIN_COLOR } from '../common_styles/color';
 import { CONTENTS_SEPARATOR_SPACE } from '../common_styles/space';
 import { SITE_WIDTH } from '../common_styles/size';
 import { SITE_TITLE, SITE_URL } from '../constants';
@@ -12,6 +13,16 @@ const SiteContents = styled.main`
   max-width: ${SITE_WIDTH};
   margin: ${CONTENTS_SEPARATOR_SPACE} auto 0;
   padding: 0 1rem;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  border-bottom: 4px solid ${MAIN_COLOR};
+  line-height: 1.4;
+`;
+
+const Contents = styled.div`
+  margin-top: calc(${CONTENTS_SEPARATOR_SPACE} / 1.5);
 `;
 
 const PrivacyPolicyPage = (): JSX.Element => {
@@ -25,19 +36,22 @@ const PrivacyPolicyPage = (): JSX.Element => {
       </Head>
       <SiteHeader />
       <SiteContents>
-        <p>当ブログでは内容の改善を目的として、Googleアナリティクスによるアクセス分析をおこなっています。</p>
-        <p>
-          Googleアナリティクスは、Cookie(クッキー)により、匿名のトラフィックデータを収集しています。
-          <br />
-          Cookieに含まれるデータは利用者の個人情報を特定しません。利用者はCookieを無効にした状態で当サイトにアクセスできます。
-        </p>
-        <p>
-          詳しくは
+        <Title>プライバシーポリシー</Title>
+        <Contents>
+          <p>当ブログでは内容の改善を目的として、Googleアナリティクスによるアクセス分析をおこなっています。</p>
+          <p>
+            Googleアナリティクスは、Cookie(クッキー)により、匿名のトラフィックデータを収集しています。
+            <br />
+            Cookieに含まれるデータは利用者の個人情報を特定しません。利用者はCookieを無効にした状態で当サイトにアクセスできます。
+          </p>
+          <p>
+            詳しくは
           <a href="https://policies.google.com/technologies/partner-sites">
-            Google のサービスを使用するサイトやアプリから収集した情報の Google による使用 – ポリシーと規約 – Google
+              Google のサービスを使用するサイトやアプリから収集した情報の Google による使用 – ポリシーと規約 – Google
           </a>
-          を参照してください。
+            を参照してください。
         </p>
+        </Contents>
       </SiteContents>
       <SiteFooter />
     </React.Fragment>
