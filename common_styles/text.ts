e1180168a2e1a693fe3f32f0fa02bcc9a1a6e816
Shota Kubota <ta2@o2p.jp>
2019-08-24 14:01:05 +0900

    feat(common_styles): add large font size style

--- common_styles/text.ts
+++ common_styles/text.ts
@@ -6,6 +6,7 @@
   - 3840px => 20.4px
 */
 export const BASE_FONT_SIZE = 'calc((100vw - 320px) / 800 + 1rem)';
+export const LARGE_FONT_SIZE = `calc(${BASE_FONT_SIZE} * 1.25)`;
 export const FONT_FAMILY = "'Hiragino Kaku Gothic ProN', YuGothic, '游ゴシック', Helvetica, sans-serif";
 export const PROGRAMMING_FONT_FAMILY = 'Menlo, Consolas, Monaco, monospace';
 export const LINE_HEIGHT = 1.8;
