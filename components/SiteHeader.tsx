63260aece14b31026aafb71d47e316fd060f4724
Shota Kubota <ta2@o2p.jp>
2019-09-04 13:11:34 +0900

    feat(SiteHeader): add category list in Header component

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -6,13 +6,19 @@ import { MAIN_COLOR, TEXT_COLOR_LIGHT } from '../common_styles/color';
 import { SITE_WIDTH } from '../common_styles/size';
 import { SPACE, CONTENTS_SEPARATOR_SPACE } from '../common_styles/space';
 import { SITE_TITLE } from '../constants';
+import entries from '../data/entries.json';
 
 const Header = styled.header`
-  display: flex;
-  justify-content: center;
   max-width: ${SITE_WIDTH};
   margin: calc(${CONTENTS_SEPARATOR_SPACE} / 2) auto 0;
   padding: 0 1rem;
+  text-align: center;
+
+  @media (min-width: 67.5rem) {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
 `;
 const Title = styled.h1`
   display: inline-block;
@@ -26,13 +32,59 @@ const StyledLink = styled.a`
   color: ${TEXT_COLOR_LIGHT};
   text-decoration: none;
 `;
+const CategoryList = styled.ul`
+  list-style: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0;
+  margin: 1rem auto 0;
 
-export const SiteHeader = (): JSX.Element => (
-  <Header>
-    <Title>
-      <Link href="/" passHref>
-        <StyledLink>{SITE_TITLE}</StyledLink>
-      </Link>
-    </Title>
-  </Header>
-);
+  @media (min-width: 67.5rem) {
+    margin: 0 0 0 auto;
+  }
+`;
+const Category = styled.li`
+  ::after {
+    content: '｜';
+    display: inline-block;
+    margin: 0 1rem;
+  }
+
+  :last-of-type::after {
+    content: '';
+    margin: 0;
+  }
+`;
+
+export const SiteHeader = (): JSX.Element => {
+  const categories = getCategories();
+
+  const e = (
+    <Header>
+      <Title>
+        <Link href="/" passHref>
+          <StyledLink>{SITE_TITLE}</StyledLink>
+        </Link>
+      </Title>
+      <CategoryList>
+        {categories.map((category, i) => (
+          <Category key={i}>
+            <Link href="/categories/[category]" as={`/categories/${category}`}>
+              {category}
+            </Link>
+          </Category>
+        ))}
+      </CategoryList>
+    </Header>
+  );
+
+  return e;
+};
+
+function getCategories(): Array<string> {
+  const uniqCategories = new Set(entries.flatMap(entry => entry.categories));
+  const categories = Array.from(uniqCategories);
+
+  return categories;
+}
