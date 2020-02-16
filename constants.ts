933389d49230617a1e511b88d348c5a7ebeddc14
Shota Kubota <ta2@o2p.jp>
2020-02-16 16:37:54 +0900

    chore(constants): change site title and description

--- constants.ts
+++ constants.ts
@@ -1,5 +1,6 @@
-export const SITE_DESCRIPTION = 'kuboshoの個人ブログです。ソフトウェア開発や、ゲームやダーツなどについて書きます。';
-export const SITE_TITLE = "I'm kubosho";
+export const SITE_DESCRIPTION =
+  '主にソフトウェアについて書きます。学習し、自分の中で落とし込んだ後、言語化をして書き出すブログです。';
+export const SITE_TITLE = '学ぶ、考える、書き出す。';
 export const SITE_URL = 'https://blog.kubosho.com';
 
 export const GA_TRACKING_ID = 'UA-25380313-10';
