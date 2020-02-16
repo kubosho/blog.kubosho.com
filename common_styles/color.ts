45e698117ded4f6b6529c2a3b7afc88d66801e49
Shota Kubota <ta2@o2p.jp>
2020-02-16 21:34:15 +0900

    feat(color): add color constants

--- common_styles/color.ts
+++ common_styles/color.ts
@@ -1,4 +1,5 @@
 // Color palette (Don't direct use for other files)
+const BLUE_200 = '#0076d1';
 const BLUE_600 = '#003760';
 
 const BEIGE_500 = '#858475';
@@ -6,6 +7,7 @@ const BEIGE_500 = '#858475';
 const YELLOW_200 = '#d4cd00';
 
 const MONOTONE_100 = '#f0f0f0';
+const MONOTONE_400 = '#9e9e9e';
 const MONOTONE_800 = '#2b2b2b';
 
 // Primary color
@@ -17,4 +19,6 @@ export const ACCENT_COLOR = YELLOW_200;
 export const BACKGROUND_COLOR = MONOTONE_100;
 export const CODE_BACKGROUND_COLOR = '#393939';
 export const TEXT_COLOR = MONOTONE_800;
-export const TEXT_COLOR_LIGHT = '#f9fcff';
+export const TEXT_COLOR_LIGHT = MONOTONE_100;
+export const LINK_COLOR = BLUE_200;
+export const BORDER_COLOR = MONOTONE_400;
