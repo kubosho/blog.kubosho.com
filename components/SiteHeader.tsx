a55edbe5eddb35d44175bacfd6f8ef811265977e
Shota Kubota <ta2@o2p.jp>
2020-02-16 20:19:55 +0900

    feat(SiteHeader): change SiteHeader style

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -2,64 +2,53 @@ import React from 'react';
 import styled from 'styled-components';
 import Link from 'next/link';
 
-import { MAIN_COLOR, TEXT_COLOR_LIGHT } from '../common_styles/color';
-import { SITE_WIDTH } from '../common_styles/size';
-import { SPACE, CONTENTS_SEPARATOR_SPACE } from '../common_styles/space';
+import { MAIN_COLOR } from '../common_styles/color';
 import { SITE_TITLE } from '../constants';
-import entries from '../data/entries.json';
 
 const Header = styled.header`
-  max-width: ${SITE_WIDTH};
-  margin: calc(${CONTENTS_SEPARATOR_SPACE} / 2) auto 0;
-  padding: 0 1rem;
+  height: calc(100vw - 45.875rem);
+  min-height: 60px;
+  max-height: 100px;
   text-align: center;
+`;
 
-  @media (min-width: 67.5rem) {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-  }
-`;
 const Title = styled.h1`
-  display: inline-block;
-  padding: 0 calc(${SPACE} * 3);
-  margin: 0;
-  background-color: ${MAIN_COLOR};
-  font-size: 1.5rem;
-  font-weight: 500;
-`;
-const StyledLink = styled.a`
-  color: ${TEXT_COLOR_LIGHT};
-  text-decoration: none;
-`;
-const CategoryList = styled.ul`
-  list-style: none;
   display: flex;
   align-items: center;
-  justify-content: center;
-  padding: 0;
-  margin: 1rem auto 0;
+  position: relative;
+  max-width: 50rem;
+  height: 100%;
+  padding-left: 20px;
+  margin: 0 auto;
+  border-left: 40px solid ${MAIN_COLOR};
+  font-size: 1.375rem;
+  font-weight: 500;
 
-  @media (min-width: 67.5rem) {
-    margin: 0 0 0 auto;
+  &::before {
+    content: '';
+    display: block;
+    position: absolute;
+    left: calc(-100vw - 40px);
+    background-color: ${MAIN_COLOR};
+    width: 100vw;
+    height: 100%;
+  }
+
+  @media (min-width: 37.5rem) {
+    font-size: calc(1.375rem + ((1vw - 0.375rem) * 5.982));
+  }
+
+  @media (min-width: 52.125rem) {
+    font-size: 2.25rem;
   }
 `;
-const Category = styled.li`
-  ::after {
-    content: '｜';
-    display: inline-block;
-    margin: 0 1rem;
-  }
 
-  :last-of-type::after {
-    content: '';
-    margin: 0;
-  }
+const StyledLink = styled.a`
+  color: ${MAIN_COLOR};
+  text-decoration: none;
 `;
 
 export const SiteHeader = (): JSX.Element => {
-  const categories = getCategories();
-
   const e = (
     <Header>
       <Title>
@@ -67,25 +56,8 @@ export const SiteHeader = (): JSX.Element => {
           <StyledLink>{SITE_TITLE}</StyledLink>
         </Link>
       </Title>
-      <CategoryList>
-        {categories.map((category, i) => (
-          <Category key={i}>
-            <Link href="/categories/[category]" as={`/categories/${category}`}>
-              {category}
-            </Link>
-          </Category>
-        ))}
-      </CategoryList>
     </Header>
   );
 
   return e;
 };
-
-function getCategories(): Array<string> {
-  const categories = entries.map(entry => entry.categories.reduce((a, c) => a.concat(c)));
-  const uniqCategories = new Set(categories);
-  const r = Array.from(uniqCategories);
-
-  return r;
-}
