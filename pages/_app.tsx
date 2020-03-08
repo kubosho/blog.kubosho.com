6c1b281014c501db733073211d61916b17758db9
Shota Kubota <ta2@o2p.jp>
2020-03-09 02:04:37 +0900

    feat(pages): move head contents from _document to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import Head from 'next/head';
 import { createGlobalStyle } from 'styled-components';
 
@@ -7,6 +7,9 @@ import { SITE_TITLE } from '../constants';
 import { BACKGROUND_COLOR, MAIN_COLOR, TEXT_COLOR } from '../common_styles/color';
 import { BASE_FONT_SIZE, FONT_FAMILY, LINE_HEIGHT, PROGRAMMING_FONT_FAMILY } from '../common_styles/text';
 import { SPACE } from '../common_styles/space';
+import { insertGtmScript } from '../tracking/gtm';
+import { PRODUCTION_GTM_ID, DEVELOPMENT_GTM_ID } from '../tracking/gtm_id';
+import { isProduction, isDevelopment } from './environment';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -38,17 +41,28 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props;
 
     return (
-      <Container>
+      <>
         <Head>
           <meta name="theme-color" content={MAIN_COLOR} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@kubosho_" />
           <link rel="apple-touch-icon" href="/static/images/icon.png" />
           <link rel="icon" type="image/png" href="/static/images/icon.png" />
           <link rel="stylesheet" href="/static/styles/foundation.css" />
           <link rel="alternate" type="application/rss+xml" href="/feed" title={SITE_TITLE} />
+          {isProduction && insertGtmScript(PRODUCTION_GTM_ID)}
+          {isDevelopment && insertGtmScript(DEVELOPMENT_GTM_ID)}
+          <script
+            async
+            defer
+            crossOrigin="anonymous"
+            src="https://connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v4.0"
+          />
+          <script async defer src="https://platform.twitter.com/widgets.js" />
         </Head>
         <GlobalStyle />
         <Component {...pageProps} />
-      </Container>
+      </>
     );
   }
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,64 +1,17 @@
 import React from 'react';
-import Document, { Main, NextScript, Head, DocumentInitialProps, DocumentContext } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
-import { SITE_TITLE } from '../constants';
+import Document, { Main, NextScript, Head } from 'next/document';
 import { insertGtmNoscript } from '../tracking/gtm_noscript';
-import { insertGtmScript } from '../tracking/gtm';
+import { PRODUCTION_GTM_ID, DEVELOPMENT_GTM_ID } from '../tracking/gtm_id';
+import { isProduction, isDevelopment } from './environment';
 
-type Props = {
-  isProduction: boolean;
-} & DocumentInitialProps;
-
-export default class MyDocument extends Document<Props> {
-  static async getInitialProps(ctx: DocumentContext): Promise<Props> {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    const isProduction = process.env.NODE_ENV === 'production';
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-
-      return {
-        ...initialProps,
-        isProduction,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
-  }
-
+export default class MyDocument extends Document {
   render(): JSX.Element {
-    const { isProduction } = this.props;
-
     return (
       <html lang="ja">
-        <Head>
-          <meta itemProp="name" content={SITE_TITLE} />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@kubosho_" />
-          <script
-            async
-            defer
-            crossOrigin="anonymous"
-            src="https://connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v4.0"
-          />
-          <script async defer src="https://platform.twitter.com/widgets.js" />
-          {isProduction && insertGtmScript()}
-        </Head>
+        <Head />
         <body>
-          {isProduction && insertGtmNoscript()}
+          {isProduction && insertGtmNoscript(PRODUCTION_GTM_ID)}
+          {isDevelopment && insertGtmNoscript(DEVELOPMENT_GTM_ID)}
           <Main />
           <NextScript />
         </body>
