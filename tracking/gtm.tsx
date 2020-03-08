3e3a54575d2887ed17ce5f27e166dc3a11094d06
Shota Kubota <ta2@o2p.jp>
2020-03-09 01:59:25 +0900

    fixup! feat(tracking/gtm): remove dangerouslySetInnerHTML

--- tracking/gtm.tsx
+++ tracking/gtm.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-export const PRODUCTION_GTM_ID = 'GTM-WQNTM9W';
-export const DEVELOPMENT_GTM_ID = 'GTM-5FH7ZXN';
-
 export function insertGtmScript(id: string): JSX.Element {
   let globalObject = null;
 
