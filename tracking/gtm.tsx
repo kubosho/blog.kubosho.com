b35a3bd23d3d5ae68a54150ae904f085e2f5cf2a
Shota Kubota <ta2@o2p.jp>
2020-03-09 01:58:31 +0900

    feat(tracking/gtm): remove dangerouslySetInnerHTML

--- tracking/gtm.tsx
+++ tracking/gtm.tsx
@@ -1,29 +1,27 @@
 import React from 'react';
 
-export function insertGtmScript(): JSX.Element {
-  return (
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `
-function runGtm(win, doc, dataLayer, id) {
+export const PRODUCTION_GTM_ID = 'GTM-WQNTM9W';
+export const DEVELOPMENT_GTM_ID = 'GTM-5FH7ZXN';
+
+export function insertGtmScript(id: string): JSX.Element {
+  let globalObject = null;
+
+  try {
+    globalObject = window;
+  } catch (_err) {
+    globalObject = global;
+  }
+
+  return runGtm(globalObject, 'dataLayer', id);
+}
+
+function runGtm(win, dataLayer, id): JSX.Element {
   win.dataLayer = win.dataLayer || [];
 
   win[dataLayer].push({
     'gtm.start': new Date().getTime(),
-    event: 'gtm.js'
+    event: 'gtm.js',
   });
 
-  var bs = doc.querySelector('script'),
-      cs = doc.createElement('script'),
-      dl = dataLayer != 'dataLayer' ? '&l=' + dataLayer : '';
-      cs.async = true;
-      cs.src = 'https://www.googletagmanager.com/gtm.js?id=' + id + dl;
-      bs.parentNode.insertBefore(cs, bs);
-}
-
-runGtm(window, document, 'dataLayer', 'GTM-WQNTM9W');
-  `,
-      }}
-    />
-  );
+  return <script src={`https://www.googletagmanager.com/gtm.js?id=${id}`} async />;
 }
