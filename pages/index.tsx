2440e7a935154fa7deb5830465cd3a0732160c3d
Shota Kubota <ta2@o2p.jp>
2020-04-23 01:41:07 +0900

    feat(pages/index): implement variable font size belong to viewport

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,7 +38,15 @@ const ArticleHeader = styled.header`
 const ArticleTitle = styled.h2`
   grid-area: title;
   margin: 0;
-  font-size: 1.5rem;
+  font-size: 1rem;
+
+  @media (min-width: 37.5rem) {
+    font-size: calc(1rem + ((1vw - 0.375rem) * 3.419));
+  }
+
+  @media (min-width: 52.125rem) {
+    font-size: 1.5rem;
+  }
 `;
 const StyledLink = styled.a`
   color: ${TEXT_COLOR};
