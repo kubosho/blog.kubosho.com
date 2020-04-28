6568c6fbddda7dfcf93aca2ad6540c8fbaccbbfa
Shota Kubota <ta2@o2p.jp>
2020-04-29 00:34:20 +0900

    feat(components): add TagList component

---
+++ components/TagList.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import { TEXT_COLOR_LIGHT, SUB_COLOR } from '../common_styles/color';
+import { NOTE_FONT_SIZE } from '../common_styles/text';
+import { SPACE } from '../common_styles/space';
+
+export const TagList = styled.ul`
+  grid-area: tags;
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  color: ${TEXT_COLOR_LIGHT};
+  font-size: ${NOTE_FONT_SIZE};
+`;
+
+export const TagListItem = styled.li`
+  padding: calc(${SPACE} / 2) calc(${SPACE} * 2);
+  margin-right: calc(${SPACE} * 3);
+  background-color: ${SUB_COLOR};
+
+  &:last-child {
+    margin-right: 0;
+  }
+
+  a:link,
+  a:visited {
+    color: ${TEXT_COLOR_LIGHT};
+    text-decoration: none;
+  }
+`;
