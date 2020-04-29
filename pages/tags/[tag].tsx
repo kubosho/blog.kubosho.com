f6caa1fb9ec0074694f696c61139a395f380c034
Shota Kubota <ta2@o2p.jp>
2020-04-29 23:30:51 +0900

    feat(pages/tags): use EntryList component

--- pages/tags/[tag].tsx
+++ pages/tags/[tag].tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import { NextPageContext } from 'next';
-import Link from 'next/link';
 
-import { CONTENTS_SEPARATOR_SPACE, SPACE } from '../../common_styles/space';
+import { CONTENTS_SEPARATOR_SPACE } from '../../common_styles/space';
 import { SITE_WIDTH } from '../../common_styles/size';
-import { LARGE_FONT_SIZE } from '../../common_styles/text';
-import { PublishedDate } from '../../components/PublishedDate';
 import { SiteHeader } from '../../components/SiteHeader';
 import { SiteFooter } from '../../components/SiteFooter';
 import { EntryValue } from '../../entry/entryValue';
+import { EntryList } from '../../entry/components/EntryList';
 import entries from '../../data/entries.json';
 
-type Props = {
+interface Props {
   filteredEntries: Array<EntryValue>;
   tag?: string;
-};
+}
 
 const SiteContents = styled.main`
   max-width: ${SITE_WIDTH};
@@ -23,26 +21,6 @@ const SiteContents = styled.main`
   padding: 0 1rem;
 `;
 const ArticlesTitle = styled.h2``;
-const Article = styled.article`
-  position: relative;
-  margin: calc(${CONTENTS_SEPARATOR_SPACE} / 2) 0;
-`;
-const StyledLink = styled.a`
-  font-size: ${LARGE_FONT_SIZE};
-`;
-const Contents = styled.p`
-  margin: 0;
-`;
-const Date = styled.div`
-  color: rgba(0, 0, 0, 0.75);
-
-  @media (min-width: 67.5rem) {
-    position: absolute;
-    top: ${SPACE};
-    left: calc(${CONTENTS_SEPARATOR_SPACE} * -1.5);
-  }
-`;
-const NotFound = styled.p``;
 
 export const TagPage = (props: Props): JSX.Element => {
   const { tag, filteredEntries } = props;
@@ -52,26 +30,7 @@ export const TagPage = (props: Props): JSX.Element => {
       <SiteHeader />
       <SiteContents>
         <ArticlesTitle>{`${tag}の記事一覧`}</ArticlesTitle>
-        {filteredEntries.length > 0 ? (
-          filteredEntries.map((entry) => {
-            const { excerpt, id, slug, title, createdAt } = entry;
-            const e = (
-              <Article key={id}>
-                <Link href="/entry/[slug]" as={`/entry/${slug}`} passHref>
-                  <StyledLink>{title}</StyledLink>
-                </Link>
-                <Contents dangerouslySetInnerHTML={{ __html: excerpt }} />
-                <Date>
-                  <PublishedDate createdAt={createdAt} />
-                </Date>
-              </Article>
-            );
-
-            return e;
-          })
-        ) : (
-          <NotFound>記事はありません。</NotFound>
-        )}
+        <EntryList entries={filteredEntries} />
       </SiteContents>
       <SiteFooter />
     </React.Fragment>
