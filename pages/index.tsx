24ea7f26091acc169ff6bf1eba9b1cbcc333f95d
Shota Kubota <ta2@o2p.jp>
2020-04-29 23:30:45 +0900

    feat(pages/index): use EntryList component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,88 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import Head from 'next/head';
-import Link from 'next/link';
-import { isNotNull } from 'option-t/lib/Nullable/Nullable';
 
 import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL } from '../constants/site_data';
 import { SITE_WIDTH } from '../common_styles/size';
+import { SPACE } from '../common_styles/space';
 import { SiteHeader } from '../components/SiteHeader';
 import { SiteFooter } from '../components/SiteFooter';
-import { PublishedDate } from '../components/PublishedDate';
-import { BORDER_COLOR, TEXT_COLOR, TEXT_COLOR_LIGHT, MAIN_COLOR } from '../common_styles/color';
+import { EntryList } from '../entry/components/EntryList';
 import entries from '../data/entries.json';
-import { NOTE_FONT_SIZE } from '../common_styles/text';
-import { SPACE } from '../common_styles/space';
-import { EntryContents } from '../entry/components/EntryContents';
 
 const SiteContents = styled.main`
   max-width: ${SITE_WIDTH};
   margin: calc(${SPACE} * 15) auto 0;
 `;
-const Article = styled.article`
-  padding: 0 calc(${SPACE} * 3) calc(${SPACE} * 6);
-  margin-bottom: calc(${SPACE} * 6);
-  border-bottom: 1px solid ${BORDER_COLOR};
-
-  @media (min-width: 52.125rem) {
-    padding: 0 0 calc(${SPACE} * 6);
-  }
-`;
-const ArticleHeader = styled.header`
-  display: grid;
-  grid-template-areas:
-    'title date'
-    'tags tags';
-  grid-template-columns: 1fr auto;
-  grid-template-rows: 1fr auto;
-`;
-const ArticleTitle = styled.h2`
-  grid-area: title;
-  margin: 0;
-  font-size: 1rem;
-
-  @media (min-width: 37.5rem) {
-    font-size: calc(1rem + ((1vw - 0.375rem) * 3.419));
-  }
-
-  @media (min-width: 52.125rem) {
-    font-size: 1.5rem;
-  }
-`;
-const StyledLink = styled.a`
-  color: ${TEXT_COLOR};
-  text-decoration: none;
-`;
-const Date = styled.div`
-  grid-area: date;
-  margin: 0 calc(${SPACE} * -10) 0 calc(${SPACE} * 3);
-  color: ${TEXT_COLOR_LIGHT};
-  font-size: ${NOTE_FONT_SIZE};
-
-  time {
-    display: inline-block;
-    padding: calc(${SPACE} / 2) calc(${SPACE} * 10) calc(${SPACE} / 2) ${SPACE};
-    background-color: ${MAIN_COLOR};
-  }
-`;
-const Excerpt = styled(EntryContents)`
-  overflow: hidden;
-  text-overflow: ellipsis;
-  margin: calc(${SPACE} * 3) 0 0;
-
-  @supports (-webkit-line-clamp: 2) {
-    /* stylelint-disable-next-line value-no-vendor-prefix */
-    display: -webkit-box;
-    /* stylelint-disable-next-line property-no-vendor-prefix */
-    -webkit-box-orient: vertical;
-    /* stylelint-disable-next-line property-no-vendor-prefix */
-    -webkit-line-clamp: 2;
-  }
-`;
-const ExcerptInner = styled.p`
-  margin: 0;
-`;
-const NotFound = styled.p``;
 
 const TopPage = (): JSX.Element => {
   const e = (
@@ -105,31 +36,7 @@ const TopPage = (): JSX.Element => {
       </Head>
       <SiteHeader />
       <SiteContents>
-        {isNotNull(entries) && entries.length > 1 ? (
-          entries.map((entry) => {
-            const { excerpt, id, slug, title, createdAt } = entry;
-
-            return (
-              <Article key={id}>
-                <ArticleHeader>
-                  <ArticleTitle>
-                    <Link href="/entry/[slug]" as={`/entry/${slug}`} passHref>
-                      <StyledLink>{title}</StyledLink>
-                    </Link>
-                  </ArticleTitle>
-                  <Date>
-                    <PublishedDate createdAt={createdAt} />
-                  </Date>
-                </ArticleHeader>
-                <Excerpt>
-                  <ExcerptInner dangerouslySetInnerHTML={{ __html: excerpt }} />
-                </Excerpt>
-              </Article>
-            );
-          })
-        ) : (
-          <NotFound>記事はありません。</NotFound>
-        )}
+        <EntryList entries={entries} />
       </SiteContents>
       <SiteFooter />
     </React.Fragment>
