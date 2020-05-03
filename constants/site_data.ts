b48e38a06d63d355cef2a38abdace24de690da0f
Shota Kubota <ta2@o2p.jp>
2020-05-03 23:52:08 +0900

    chore(constants): add Twitter account ID

--- constants/site_data.ts
+++ constants/site_data.ts
@@ -2,4 +2,5 @@ export const SITE_DESCRIPTION =
   '主にソフトウェアについて書きます。学習し、自分の中で落とし込んだ後、言語化をして書き出すブログです。';
 export const SITE_TITLE = '学ぶ、考える、書き出す。';
 export const SITE_URL = 'https://blog.kubosho.com';
+export const TWITTER_ACCOUNT_ID = 'kubosho_';
 export const FACEBOOK_APP_ID = '2453282784920956';
