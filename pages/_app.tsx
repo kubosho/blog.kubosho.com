8b4ad3b84385c82842fb232e81dfee8f0f2f2694
Shota Kubota <ta2@o2p.jp>
2020-05-06 23:21:04 +0900

    feat(common_styles): convert to styled-components for foundation.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import App from 'next/app';
 import Head from 'next/head';
-import { createGlobalStyle } from 'styled-components';
 
 import { SITE_TITLE } from '../constants/site_data';
-import { BACKGROUND_COLOR, MAIN_COLOR, TEXT_COLOR } from '../common_styles/color';
-import { BASE_FONT_SIZE, FONT_FAMILY, LINE_HEIGHT, PROGRAMMING_FONT_FAMILY } from '../common_styles/text';
-import { SPACE } from '../common_styles/space';
+import { MAIN_COLOR } from '../common_styles/color';
 import { insertGtmScript } from '../tracking/gtm';
 import { PRODUCTION_GTM_ID, DEVELOPMENT_GTM_ID } from '../tracking/gtm_id';
 import { isProduction, isDevelopment } from '../constants/environment';
@@ -15,35 +12,7 @@ import { activateBugsnag } from '../error_reporter/activate_bugsnag';
 import { activateErrorBoundaryComponent } from '../components/ErrorBoundary';
 import { SiteHeader } from '../components/SiteHeader';
 import { SiteFooter } from '../components/SiteFooter';
-
-const GlobalStyle = createGlobalStyle`
-  html {
-    overflow-x: hidden;
-  }
-
-  body {
-    background-color: ${BACKGROUND_COLOR};
-    color: ${TEXT_COLOR};
-    font-family: ${FONT_FAMILY};
-    font-weight: 500;
-    font-feature-settings: 'palt';
-    text-rendering: optimizeLegibility;
-    font-size: ${BASE_FONT_SIZE};
-    line-height: ${LINE_HEIGHT};
-    overflow: hidden;
-  }
-
-  code {
-    padding: 0 ${SPACE};
-    border: 1px dotted ${MAIN_COLOR};
-    font-family: ${PROGRAMMING_FONT_FAMILY};
-  }
-
-  pre code {
-    padding: 0;
-    border: none;
-  }
-`;
+import { FoundationStyles } from '../common_styles/foundation';
 
 const gtmId = isProduction ? PRODUCTION_GTM_ID : DEVELOPMENT_GTM_ID;
 const gaOptout = createGAOptout(gtmId);
@@ -63,12 +32,11 @@ export default class MyApp extends App {
           <meta name="twitter:site" content="@kubosho_" />
           <link rel="apple-touch-icon" href="/static/images/icon.png" />
           <link rel="icon" type="image/png" href="/static/images/icon.png" />
-          <link rel="stylesheet" href="/static/styles/foundation.css" />
           <link rel="alternate" type="application/rss+xml" href="/feed" title={SITE_TITLE} />
           {!gaOptout.enabled() && isProduction && insertGtmScript(PRODUCTION_GTM_ID)}
           {!gaOptout.enabled() && isDevelopment && insertGtmScript(DEVELOPMENT_GTM_ID)}
         </Head>
-        <GlobalStyle />
+        <FoundationStyles />
         <SiteHeader />
         <Component {...pageProps} />
         <SiteFooter />
