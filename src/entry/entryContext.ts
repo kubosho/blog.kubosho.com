9f3cbcc5cfdeed9cc45c21cfc96c96219057445c
Shota Kubota <ta2@o2p.jp>
2020-05-15 01:33:15 +0900

    feat(entry): remove unnecessary files

--- src/entry/entryContext.ts
+++
@@ -1,6 +0,0 @@
-import { createContentfulClient } from './contentfulClient';
-import { createEntryGateway } from './entryGateway';
-
-const client = createContentfulClient(process.env.SPACE, process.env.ACCESS_TOKEN);
-
-export const entryGateway = createEntryGateway(client);
