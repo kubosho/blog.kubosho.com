953fda1497abaf73b8b837f113ecf5e12a432b07
Shota Kubota <ta2@o2p.jp>
2020-06-19 15:53:55 +0900

    feat(constants): add local environment URL

--- src/constants/site_data.ts
+++ src/constants/site_data.ts
@@ -1,6 +1,9 @@
 export const SITE_DESCRIPTION =
   '主にソフトウェアについて、学習し自分の中で落とし込んだあとに言語化をして書き出すブログです。';
 export const SITE_TITLE = '学ぶ、考える、書き出す。';
+
 export const SITE_URL = 'https://blog.kubosho.com';
+export const LOCAL_SITE_URL = 'http://localhost:3000';
+
 export const TWITTER_ACCOUNT_ID = 'kubosho_';
 export const FACEBOOK_APP_ID = '2453282784920956';
