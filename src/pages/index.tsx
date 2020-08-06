2b2ccbd932cc8e2f82ae3bd1fe83f3d6ad8a892d
Shota Kubota <ta2@o2p.jp>
2020-08-07 00:11:40 +0900

    feat(pages/index): add page description in top page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 import Head from 'next/head';
 
 import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL } from '../constants/site_data';
 import { EntryList } from '../entry/components/EntryList';
+import { PageDescription } from '../components/PageDescription';
 import { SiteContents } from '../components/SiteContents';
 import { EntryValue } from '../entry/entryValue';
 import { fetchEntries } from '../entry/entryGateway';
+import { CONTENTS_SEPARATOR_SPACE, SPACE } from '../common_styles/space';
 
 interface Props {
   entries: Array<EntryValue>;
 }
+
+const ModifiedPageDescription = styled(PageDescription)`
+  padding: 0 calc(${SPACE} * 3);
+  margin: 0 auto ${CONTENTS_SEPARATOR_SPACE};
+
+  @media (min-width: 52.125rem) {
+    padding: 0;
+    margin: 0 auto calc(${CONTENTS_SEPARATOR_SPACE} * 1.5);
+  }
+`;
 
 const TopPage = (props: Props): JSX.Element => {
   const { entries } = props;
@@ -25,6 +38,7 @@ const TopPage = (props: Props): JSX.Element => {
         <meta property="og:type" content="website" />
       </Head>
       <SiteContents>
+        <ModifiedPageDescription>{SITE_DESCRIPTION}</ModifiedPageDescription>
         <EntryList entries={entries} />
       </SiteContents>
     </React.Fragment>
