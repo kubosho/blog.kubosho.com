ff0518fd21784eeea03fc17d652bb4c66aac8291
Shota Kubota <ta2@o2p.jp>
2020-08-06 23:36:53 +0900

    feat(constants): change site description

--- src/constants/site_data.ts
+++ src/constants/site_data.ts
@@ -1,5 +1,4 @@
-export const SITE_DESCRIPTION =
-  '主にソフトウェアについて、学習し自分の中で落とし込んだあとに言語化をして書き出すブログです。';
+export const SITE_DESCRIPTION = '学習し、自分の中で落とし込んだあとに、言語化をして書き出すブログ。';
 export const SITE_TITLE = '学ぶ、考える、書き出す。';
 
 export const SITE_URL = 'https://blog.kubosho.com';
