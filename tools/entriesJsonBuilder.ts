077879d7979104d58f8206562d34162f19ab14e7
Shota Kubota <ta2@o2p.jp>
2020-08-15 00:24:48 +0900

    feat(entriesJsonBuilder): add ignoreDir argument to readEntryList()

--- tools/entriesJsonBuilder.ts
+++ tools/entriesJsonBuilder.ts
@@ -11,14 +11,16 @@ const writeFile = promisify(legacyWriteFile);
 const BASE_DIR = pathJoin(__dirname, '..');
 
 const SRC_DIR = `${BASE_DIR}/entries`;
+const DRAFT_DIR = `${BASE_DIR}/entries/draft`;
 
 const DIST_DIR = 'data';
 const OUTPUT_FILE = 'entries.json';
 const DESTINATION_FILE = `${BASE_DIR}/${DIST_DIR}/${OUTPUT_FILE}`;
 
-async function readEntryList(sourceDir: string): Promise<Array<EntryValue>> {
+async function readEntryList(sourceDir: string, ignoreDir?: string): Promise<Array<EntryValue>> {
   const markdownFiles = await retrieveMarkdownFiles(sourceDir);
-  const entriesData = markdownFiles.map(async (file) => await readMarkdownFileData(file));
+  const filteredMarkdownFiles = markdownFiles.filter((filepath) => !filepath.includes(ignoreDir));
+  const entriesData = filteredMarkdownFiles.map(async (file) => await readMarkdownFileData(file));
   const params = entriesData.map((data) => data.then(mapEntryValueParameter));
   const values = params.map((param) => param.then((p) => new EntryValue(p)));
   const entries = await Promise.all(values);
@@ -29,7 +31,7 @@ async function readEntryList(sourceDir: string): Promise<Array<EntryValue>> {
 export async function main(): Promise<void> {
   const spinner = ora('Entry list create staring.').start();
 
-  const entryList = (await readEntryList(SRC_DIR))
+  const entryList = (await readEntryList(SRC_DIR, DRAFT_DIR))
     .sort((e1, e2) => e2.createdAt - e1.createdAt)
     .map((entry) => [entry.id, entry]);
   const entries = Object.fromEntries(entryList);
