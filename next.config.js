4cb33f871f45810886a0f5a00668bcfec0702b09
Shota Kubota <ta2@o2p.jp>
2020-08-27 00:37:31 +0900

    build(nextconfig): remove preact config

--- next.config.js
+++ next.config.js
@@ -8,43 +8,6 @@ const config = {
   env: {
     BUGSNAG_API_KEY: process.env.BUGSNAG_API_KEY,
   },
-  webpack(config, { dev, isServer }) {
-    const splitChunks = config.optimization && config.optimization.splitChunks;
-
-    if (splitChunks) {
-      const cacheGroups = splitChunks.cacheGroups;
-      const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
-
-      if (cacheGroups.framework) {
-        cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
-          test: preactModules,
-        });
-        cacheGroups.commons.name = 'framework';
-      } else {
-        cacheGroups.preact = {
-          name: 'commons',
-          chunks: 'all',
-          test: preactModules,
-        };
-      }
-    }
-
-    // Install webpack aliases:
-    const aliases = config.resolve.alias || (config.resolve.alias = {});
-    aliases.react = aliases['react-dom'] = 'preact/compat';
-
-    // inject Preact DevTools
-    if (dev && !isServer) {
-      const entry = config.entry;
-      config.entry = () =>
-        entry().then((entries) => {
-          entries['main.js'] = ['preact/debug'].concat(entries['main.js'] || []);
-          return entries;
-        });
-    }
-
-    return config;
-  },
 };
 
 module.exports = withBundleAnalyzer(config);
