f1cde24e3f2d23e5b94528208f7286ae8d42da67
Shota Kubota <ta2@o2p.jp>
2020-12-05 11:04:42 +0900

    fixup! feat(page/entry): separate entry page style

--- src/pages/entry/[id].tsx
+++ src/pages/entry/[id].tsx
@@ -1,13 +1,12 @@
 import React, { useEffect } from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 import { GetStaticPropsContext } from 'next';
 import Head from 'next/head';
 import { isNotUndefined } from 'option-t/lib/Undefinable/Undefinable';
 
 import { EntryValue } from '../../entry/entryValue';
 import { SITE_URL } from '../../constants/site_data';
-import { ACCENT_COLOR, CODE_BACKGROUND_COLOR, MAIN_COLOR, TEXT_COLOR_LIGHT } from '../../common_styles/color';
-import { CONTENTS_SEPARATOR_SPACE, SPACE } from '../../common_styles/space';
+import { CONTENTS_SEPARATOR_SPACE } from '../../common_styles/space';
 import { PublishedDate } from '../../components/PublishedDate';
 import { EntryTagList } from '../../entry/components/EntryTagList';
 import { EntryContents } from '../../entry/components/EntryContents';
@@ -17,6 +16,7 @@ import { EntryFooter } from '../../entry/components/EntryFooter';
 import { SiteContents } from '../../components/SiteContents';
 import { addSiteTitleToSuffix } from '../../site_title_inserter';
 import { getEntry, getEntryIdList } from '../../entry/entryGateway';
+import { EntryContentsStyle } from '../../entry/components/EntryContentsStyle';
 
 declare global {
   interface Window {
@@ -76,80 +76,6 @@ const EntryTags = styled(EntryTagList)`
   margin: calc(calc(1rem * 5) / 2) 0 !important;
 `;
 
-const GlobalStyle = createGlobalStyle`
-  blockquote, p, pre, ol, ul {
-    margin: calc(1rem + ${SPACE}) 0;
-  }
-
-  blockquote {
-    position: relative;
-    padding: ${SPACE} calc(1rem + ${SPACE});
-    border: 1px solid ${ACCENT_COLOR};
-  }
-
-  blockquote > p {
-    margin: 0;
-  }
-
-  blockquote::before,
-  blockquote::after {
-    position: absolute;
-    height: 0;
-    color: ${MAIN_COLOR};
-    font-size: 3rem;
-    font-weight: 900;
-    line-height: 1;
-  }
-
-  blockquote::before {
-    content: '❝';
-    top: 0.3rem;
-    right: calc(100% - 1.25rem);
-  }
-
-  blockquote::after {
-    content: '❞';
-    bottom: 2rem;
-    left: calc(100% - 1.25rem);
-  }
-
-  h2 {
-    margin: calc(${CONTENTS_SEPARATOR_SPACE} / 2) 0 0;
-  }
-
-  h3 {
-    margin: calc(${CONTENTS_SEPARATOR_SPACE} / 3) 0 0;
-  }
-
-  h4 {
-    margin: calc(${CONTENTS_SEPARATOR_SPACE} / 4) 0 0;
-  }
-
-  img {
-    max-width: 100%;
-  }
-
-  pre {
-    padding: calc(${SPACE} * 2);
-    background-color: ${CODE_BACKGROUND_COLOR};
-    color: ${TEXT_COLOR_LIGHT};
-    overflow: auto;
-  }
-
-  pre > code {
-    white-space: pre-wrap;
-  }
-
-  ol ol,
-  ul ul {
-    margin: calc((1rem + ${SPACE}) / 2) 0;
-  }
-
-  .twitter-tweet {
-    width: auto !important;
-  }
-`;
-
 const Entry = (props: Props): JSX.Element => {
   const { id, title, body, excerpt, tags, createdAt } = props.entry;
   const pageTitle = addSiteTitleToSuffix(title);
@@ -179,7 +105,7 @@ const Entry = (props: Props): JSX.Element => {
         <script defer src="https://connect.facebook.net/en_US/sdk.js" />
         <script defer src="https://platform.twitter.com/widgets.js" />
       </Head>
-      <GlobalStyle />
+      <EntryContentsStyle />
       <MainContents>
         <Container key={id}>
           <Header>
