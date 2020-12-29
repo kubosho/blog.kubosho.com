9763df379bb6ead2f34a820a4e024cc98df9cbd7
Shota Kubota <ta2@o2p.jp>
2020-12-29 17:19:27 +0900

    chore(constants): remove unused file

--- src/constants/api_url.ts
+++
@@ -1,5 +0,0 @@
-import { isProduction } from './environment';
-import { LOCAL_SITE_URL, SITE_URL } from './site_data';
-
-const API_V1_PATH = 'api/v1';
-export const API_V1_URL = isProduction ? `${SITE_URL}/${API_V1_PATH}` : `${LOCAL_SITE_URL}/${API_V1_PATH}`;
