7a29b5e2d4a7e8a7c656fd39c903e91564d7953e
Shota Kubota <ta2@o2p.jp>
2021-01-05 18:00:50 +0900

    feat(document): add GTM script insert process when disabled JavaScript

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Document, { Html, Main, NextScript, Head, DocumentInitialProps } from 'next/document';
 import { FACEBOOK_APP_ID } from '../constants/site_data';
+import { GTM_ID } from '../tracking/gtm_id';
+import { createGAOptout } from '../tracking/ga_optout';
+import { insertGtmNoscript } from '../tracking/gtm';
 
 type Props = {
   styles: JSX.Element;
@@ -17,6 +20,8 @@ const sdkInitialScript = `
   };
 `;
 
+const gaOptout = createGAOptout(GTM_ID);
+
 export default class MyDocument extends Document<Props> {
   render(): JSX.Element {
     return (
@@ -28,6 +33,7 @@ export default class MyDocument extends Document<Props> {
           <link rel="preconnect" href="https://www.googletagmanager.com" crossOrigin="" />
         </Head>
         <body>
+          {!gaOptout.enabled() && insertGtmNoscript(GTM_ID)}
           <script dangerouslySetInnerHTML={{ __html: sdkInitialScript }} />
           <Main />
           <NextScript />
