d688968cf96d43ec4068c13873c5178dc22161e9
Shota Kubota <ta2@o2p.jp>
2021-01-05 17:59:55 +0900

    refactor(app): cleanup GTM script insert process

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,9 +15,9 @@ import {
   SITE_TITLE,
   TWITTER_ACCOUNT_ID,
 } from '../constants/site_data';
+import { GTM_ID } from '../tracking/gtm_id';
 import { insertGtmScript } from '../tracking/gtm';
-import { PRODUCTION_GTM_ID, DEVELOPMENT_GTM_ID } from '../tracking/gtm_id';
-import { IS_PRODUCTION_ENV, IS_DEVELOPMENT_ENV, BUGSNAG_API_KEY } from '../constants/environment';
+import { BUGSNAG_API_KEY } from '../constants/environment';
 import { createGAOptout } from '../tracking/ga_optout';
 import { activateErrorBoundaryComponent } from '../components/ErrorBoundary';
 
@@ -31,8 +31,7 @@ import { PathList } from '../constants/path_list';
 const BLUE_600 = '#003760';
 const MAIN_COLOR = BLUE_600;
 
-const gtmId = IS_PRODUCTION_ENV ? PRODUCTION_GTM_ID : DEVELOPMENT_GTM_ID;
-const gaOptout = createGAOptout(gtmId);
+const gaOptout = createGAOptout(GTM_ID);
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const router = useRouter();
@@ -55,8 +54,7 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
         <link rel="apple-touch-icon" href={FAVICON_URL} />
         <link rel="icon" type="image/png" href={FAVICON_URL} />
         <link rel="alternate" type="application/rss+xml" href={PathList.Feed} title={SITE_TITLE} />
-        {!gaOptout.enabled() && IS_PRODUCTION_ENV && insertGtmScript(PRODUCTION_GTM_ID)}
-        {!gaOptout.enabled() && IS_DEVELOPMENT_ENV && insertGtmScript(DEVELOPMENT_GTM_ID)}
+        {!gaOptout.enabled() && insertGtmScript(GTM_ID)}
       </Head>
       <header className={styles['site-header']}>
         <h1 className={styles['site-title']}>
