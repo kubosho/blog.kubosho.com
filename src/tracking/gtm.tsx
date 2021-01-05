3c43d74fbaf43d55f2c5713ebcb796f5c4c67472
Shota Kubota <ta2@o2p.jp>
2021-01-05 18:01:27 +0900

    refactor(tracking/gtm): move insertGtmNoscript() to tracking/gtm

--- src/tracking/gtm.tsx
+++ src/tracking/gtm.tsx
@@ -16,6 +16,19 @@ export function insertGtmScript(id: string): JSX.Element {
   return <script src={`https://www.googletagmanager.com/gtm.js?id=${id}`} async />;
 }
 
+export function insertGtmNoscript(id: string): JSX.Element {
+  return (
+    <noscript>
+      <iframe
+        src={`https://www.googletagmanager.com/ns.html?id=${id}`}
+        height="0"
+        width="0"
+        style={{ display: 'none', visibility: 'hidden' }}
+      />
+    </noscript>
+  );
+}
+
 export function setDataToGtmDataLayer(param: unknown): void {
   const win = getBrowsingContextWindowProxy();
 
