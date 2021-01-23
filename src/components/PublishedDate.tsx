bcc1d02ea9b0582025236db24556364b4d0b646c
Shota Kubota <ta2@o2p.jp>
2021-01-23 20:30:28 +0900

    fix(PublishedDate): add dateTime props

--- src/components/PublishedDate.tsx
+++ src/components/PublishedDate.tsx
@@ -3,9 +3,12 @@ import React, { ReactNode, TimeHTMLAttributes } from 'react';
 import styles from './PublishedDate.module.css';
 
 interface Props {
+  dateTime: string;
   children?: ReactNode;
 }
 
-export const PublishedDate = ({ children }: TimeHTMLAttributes<Props>): JSX.Element => (
-  <time className={styles['published-date']}>{children}</time>
+export const PublishedDate = ({ dateTime, children }: TimeHTMLAttributes<Props>): JSX.Element => (
+  <time dateTime={dateTime} className={styles['published-date']}>
+    {children}
+  </time>
 );
