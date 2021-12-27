91af6b6707a0c8894ce4189d7502d1a242edb0b4
Shota Kubota <ta2@o2p.jp>
2021-12-27 20:32:31 +0900

    feat(src): implemented function returns the API response

---
+++ src/microcms_api/api_response.ts
@@ -0,0 +1,28 @@
+import { request, RequestOptions } from 'https';
+
+export async function getApiResponse<JsonType>(options: RequestOptions): Promise<JsonType> {
+  return new Promise((resolve, reject) => {
+    const req = request(options, (res) => {
+      if (res.statusCode >= 500) {
+        throw new Error(`Status Code: ${res.statusCode}`);
+      }
+
+      if (res.statusCode >= 300) {
+        return reject(new Error(`Status Code: ${res.statusCode}`));
+      }
+
+      const responseBuffer = [];
+
+      res.on('data', (d) => {
+        responseBuffer.push(d);
+      });
+
+      res.on('end', () => {
+        const resString = Buffer.concat(responseBuffer).toString();
+        resolve(JSON.parse(resString));
+      });
+    });
+
+    req.end();
+  });
+}
