fe162d04e1b184b8062d39db369891d2442a4620
Shota Kubota <ta2@o2p.jp>
2021-12-31 17:02:41 +0900

    chore(prettier): add markdown-nocjsp config

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,12 @@ module.exports = {
   singleQuote: true,
   tabWidth: 2,
   trailingComma: 'all',
+  overrides: [
+    {
+      files: ['*.md'],
+      options: {
+        parser: 'markdown-nocjsp',
+      },
+    },
+  ],
 };
