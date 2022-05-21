e8a1884b4615a4ad43ca4b5ba38a075c7a4539f6
Shota Kubota <ta2@o2p.jp>
2022-05-22 01:16:40 +0900

    feat(*): udpate unified package

--- src/entry/entryConverter.ts
+++ src/entry/entryConverter.ts
@@ -1,4 +1,4 @@
-import unified from 'unified';
+import { Processor, unified } from 'unified';
 import gfm from 'remark-gfm';
 import markdown from 'remark-parse';
 import stringify from 'remark-stringify';
@@ -15,7 +15,7 @@ import { EntryValue, EntryValueParameter } from './entryValue';
 export async function mapEntryValue(contents: EntryValueParameter): Promise<EntryValue> {
   const { title, body: originalBody, excerpt: originalExcerpt, categories, tags } = contents;
 
-  const markdownProcessor = (): unified.Processor<unified.Settings> => unified().use(markdown).use(gfm);
+  const markdownProcessor = (): Processor => unified().use(markdown).use(gfm);
   const contentsProcessor = markdownProcessor()
     .use(breaks)
     .use(remarkToRehype, { allowDangerousHtml: true })
@@ -26,7 +26,7 @@ export async function mapEntryValue(contents: EntryValueParameter): Promise<Entr
   const excerptProcessor = markdownProcessor().use(strip).use(stringify);
 
   const body = await contentsProcessor.process(originalBody);
-  const excerpt = await excerptProcessor.process({ contents: originalBody.split('\n')[0].replace(/\\$/, '') });
+  const excerpt = await excerptProcessor.process(originalBody.split('\n')[0].replace(/\\$/, ''));
 
   const categoryList = categories?.map((category) => category.trim()) ?? [];
   const tagList = tags?.map((tag) => tag.trim()) ?? [];
@@ -34,8 +34,8 @@ export async function mapEntryValue(contents: EntryValueParameter): Promise<Entr
   return new EntryValue({
     ...contents,
     title,
-    body: body.contents.toString(),
-    excerpt: originalExcerpt ?? excerpt.contents.toString().trim(),
+    body: body.value.toString(),
+    excerpt: originalExcerpt ?? excerpt.value.toString().trim(),
     categories: categoryList,
     tags: tagList,
   });
