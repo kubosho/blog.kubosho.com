8283a46260e7426640bdee177aff324370f2541a
Shota Kubota <ta2@o2p.jp>
2022-06-11 22:28:24 +0900

    feat(locales): add ja locales

---
+++ src/locales/ja.ts
@@ -0,0 +1,89 @@
+export const localeJa = {
+  website: {
+    title: '学ぶ、考える、書き出す。',
+    description: '学習し、自分なりに噛み砕いて、書き出すブログ。',
+    author: 'kubosho',
+  },
+  navigation: {
+    feed: 'フィード',
+    policy: 'ポリシー',
+  },
+  optout: {
+    actions: {
+      enabled: 'オプトアウトの有効化',
+      disabled: 'オプトアウトの無効化',
+    },
+    status: {
+      enabled: 'オプトアウトが有効になっています。Googleアナリティクスによるアクセス解析はおこなわれません。',
+      disabled: 'オプトアウトが無効になっています。Googleアナリティクスによるアクセス解析がおこなわれます。',
+    },
+  },
+  components: {
+    entryList: {
+      notFound: '記事はありません。',
+    },
+    icon: {
+      facebook: 'Facebookでシェア',
+      twitter: 'Twitterでシェア',
+    },
+  },
+  top: {
+    headings: {
+      entryList: '記事一覧',
+    },
+  },
+  draft: {
+    notAvailable: 'プレビューは利用できません。',
+  },
+  entry: {
+    share: '記事を共有する',
+    headings: {
+      related: '関連記事',
+    },
+  },
+  categories: {
+    description: '{{webSiteTitle}}の「{{category}}」に関連した記事の一覧です。',
+    headings: {
+      entryList: 'カテゴリー「{{category}}」の記事一覧',
+    },
+  },
+  tags: {
+    description: '{{webSiteTitle}}の「{{tag}}」に関連した記事の一覧です。',
+    headings: {
+      entryList: 'タグ「{{tag}}」の記事一覧',
+    },
+  },
+  policy: {
+    title: 'ポリシー',
+    headings: {
+      affiliate: 'アフィリエイト',
+      disclaimer: '免責事項',
+      optout: 'Googleアナリティクスによる解析のオプトアウト',
+      privacy: 'プライバシー',
+    },
+    intro: {
+      text: 'このページでは当ブログ『{{webSiteTitle}}』内で適用されるポリシーについて書きます。',
+    },
+    text: {
+      privacy: {
+        1: '当ブログでは内容の改善を目的として、Googleアナリティクスによるアクセス分析をおこなっています。',
+        2: 'Googleアナリティクスは、Cookie(クッキー)により、匿名のトラフィックデータを収集しています。',
+        3: 'Cookieに含まれるデータは利用者の個人情報を特定しません。利用者はCookieを無効にした状態で当ブログにアクセスできます。',
+        4: '詳しくはGoogleが公開している',
+        5: 'Google のサービスを使用するサイトやアプリから収集した情報の Google による使用',
+        6: 'のページを参照してください。',
+      },
+      optout: {
+        1: '以下のボタンからGoogleアナリティクスによる解析のオプトアウトの有効化・無効化がおこなえます。',
+      },
+      affiliate: {
+        1: '『{{webSiteTitle}}』は、Amazon.co.jpを宣伝しリンクすることによってサイトが紹介料を獲得できる手段を提供することを目的に設定されたアフィリエイトプログラムである、Amazonアソシエイト・プログラムの参加者です。',
+      },
+      disclaimer: {
+        1: '当ブログではコンテンツ・情報について、できる限り正確な情報を提供するように努めています。',
+        2: 'しかし、完全な正確性や安全性は保障いたしません。情報が古くなったり間違っていたりすることがあります。',
+        3: 'また、当ブログに掲載した内容によって生じた損害などの一切の責任は負いません。ご了承ください。',
+      },
+    },
+  },
+};
