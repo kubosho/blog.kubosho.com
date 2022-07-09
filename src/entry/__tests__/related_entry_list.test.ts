5797888e9b13f9d43e859fb96ba5dca19f284b54
Shota Kubota <ta2@o2p.jp>
2022-07-09 19:01:12 +0900

    test(related_entry_list): fix test contents

--- src/entry/__tests__/related_entry_list.test.ts
+++ src/entry/__tests__/related_entry_list.test.ts
@@ -3,67 +3,46 @@ import { EntryValue } from '../entry_value';
 import { getRelatedEntryList } from '../related_entry_list';
 
 test('getRelatedEntryList', async () => {
-  const entryListByCategory = [
-    new EntryValue({
-      id: 'hello-world',
-      slug: 'hello-world',
-      title: 'Hello, world',
-      body: '<p>こんにちは、世界！</p>',
-      excerpt: 'こんにちは、世界！',
-      categories: ['foo', 'bar', 'test'],
-      tags: ['hello', 'world', 'test'],
-      heroImage: undefined,
-      originalCreatedAt: undefined,
-      originalRevisedAt: undefined,
-      createdAt: '2020-05-13T15:34:51.620Z',
-      updatedAt: '2020-05-13T16:32:47.520Z',
-      publishedAt: '2020-05-13T15:34:51.620Z',
-      revisedAt: '2020-05-13T16:32:47.520Z',
-    }),
-    new EntryValue({
-      id: 'foo-bar',
-      slug: 'foo-bar',
-      title: 'Hello, world',
-      body: '<p>こんにちは、世界！</p>',
-      excerpt: 'こんにちは、世界！',
-      categories: ['foo', 'bar', 'test'],
-      tags: ['hello', 'world', 'test'],
-      heroImage: undefined,
-      originalCreatedAt: undefined,
-      originalRevisedAt: undefined,
-      createdAt: '2020-05-13T15:34:51.620Z',
-      updatedAt: '2020-05-13T16:32:47.520Z',
-      publishedAt: '2020-05-13T15:34:51.620Z',
-      revisedAt: '2020-05-13T16:32:47.520Z',
-    }),
-  ];
+  const mockEntryValue1 = new EntryValue({
+    id: 'hello-world',
+    slug: 'hello-world',
+    title: 'Hello, world',
+    body: '<p>こんにちは、世界！</p>',
+    excerpt: 'こんにちは、世界！',
+    categories: ['foo', 'bar', 'test'],
+    tags: ['hello', 'world', 'test'],
+    heroImage: undefined,
+    originalCreatedAt: undefined,
+    originalRevisedAt: undefined,
+    createdAt: '2020-05-13T15:34:51.620Z',
+    updatedAt: '2020-05-13T16:32:47.520Z',
+    publishedAt: '2020-05-13T15:34:51.620Z',
+    revisedAt: '2020-05-13T16:32:47.520Z',
+  });
 
-  const entryListByTags = [
-    new EntryValue({
-      id: 'foo-bar',
-      slug: 'foo-bar',
-      title: 'Hello, world',
-      body: '<p>こんにちは、世界！</p>',
-      excerpt: 'こんにちは、世界！',
-      categories: ['foo', 'bar', 'test'],
-      tags: ['hello', 'world', 'test'],
-      heroImage: undefined,
-      originalCreatedAt: undefined,
-      originalRevisedAt: undefined,
-      createdAt: '2020-05-13T15:34:51.620Z',
-      updatedAt: '2020-05-13T16:32:47.520Z',
-      publishedAt: '2020-05-13T15:34:51.620Z',
-      revisedAt: '2020-05-13T16:32:47.520Z',
-    }),
-  ];
+  const mockEntryValue2 = new EntryValue({
+    id: 'example',
+    slug: 'example',
+    title: 'Example',
+    body: '<p>サンプルです</p>',
+    excerpt: '要約のサンプルです',
+    categories: ['foo', 'bar', 'test'],
+    tags: ['example', 'test'],
+    heroImage: undefined,
+    originalCreatedAt: undefined,
+    originalRevisedAt: undefined,
+    createdAt: '2020-05-13T15:34:51.620Z',
+    updatedAt: '2020-05-13T16:32:47.520Z',
+    publishedAt: '2020-05-13T15:34:51.620Z',
+    revisedAt: '2020-05-13T16:32:47.520Z',
+  });
 
-  const actualValue = await getRelatedEntryList('hello-world', entryListByCategory, entryListByTags);
-  const expectValue = [
-    {
-      id: 'foo-bar',
-      title: 'Hello, world',
-    },
-  ];
+  const mockEntryId = 'mock-entry-id';
+  const mockEntryListByCategory = [mockEntryValue1];
+  const mockEntryListByTags = [mockEntryValue2];
+
+  const actualValue = await getRelatedEntryList(mockEntryId, mockEntryListByCategory, mockEntryListByTags);
+  const expectValue = [...mockEntryListByTags, ...mockEntryListByCategory];
 
   expect(actualValue).toStrictEqual(expectValue);
 });
