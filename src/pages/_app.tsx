2315a94030119e99b317e2c7e0889751862fccf1
Shota Kubota <ta2@o2p.jp>
2022-08-11 20:13:18 +0900

    feat(app): implement sticky footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -40,41 +40,43 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const element = (
     <>
       <SiteMetadata />
-      <header className="web-site-header">
-        <h1 className="web-site-title">
-          <Link href={PathList.Root} passHref>
-            <a dangerouslySetInnerHTML={{ __html: title }} />
-          </Link>
-        </h1>
-        <p className="web-site-description">{retrieveTranslation('website.description')}</p>
-      </header>
-      <main className="web-site-contents">
-        <Component {...pageProps} />
-      </main>
-      <footer className="web-site-footer">
-        <div className="web-site-links">
-          <p>
+      <div className="web-site-container">
+        <header className="web-site-header">
+          <h1 className="web-site-title">
             <Link href={PathList.Root} passHref>
-              <a>{retrieveTranslation('website.title')}</a>
+              <a dangerouslySetInnerHTML={{ __html: title }} />
             </Link>
+          </h1>
+          <p className="web-site-description">{retrieveTranslation('website.description')}</p>
+        </header>
+        <main className="web-site-contents">
+          <Component {...pageProps} />
+        </main>
+        <footer className="web-site-footer">
+          <div className="web-site-links">
+            <p>
+              <Link href={PathList.Root} passHref>
+                <a>{retrieveTranslation('website.title')}</a>
+              </Link>
+            </p>
+            <ul className="web-site-navigation">
+              <li>
+                <Link href={PathList.Feed}>
+                  <a>{retrieveTranslation('navigation.feed')}</a>
+                </Link>
+              </li>
+              <li>
+                <Link href={PathList.Policy}>
+                  <a>{retrieveTranslation('navigation.policy')}</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <p className="web-site-copyright">
+            <small>© {retrieveTranslation('website.author')}</small>
           </p>
-          <ul className="web-site-navigation">
-            <li>
-              <Link href={PathList.Feed}>
-                <a>{retrieveTranslation('navigation.feed')}</a>
-              </Link>
-            </li>
-            <li>
-              <Link href={PathList.Policy}>
-                <a>{retrieveTranslation('navigation.policy')}</a>
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <p className="web-site-copyright">
-          <small>© {retrieveTranslation('website.author')}</small>
-        </p>
-      </footer>
+        </footer>
+      </div>
     </>
   );
 
