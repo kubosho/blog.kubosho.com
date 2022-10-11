325a8054962d9264ac9a7e79f9ff61b0aa7f0fd0
Shota Kubota <ta2@o2p.jp>
2022-10-11 22:33:41 +0900

    feat(pages/entries): implement all entries page

---
+++ src/pages/entries/index.tsx
@@ -0,0 +1,43 @@
+import Head from 'next/head';
+
+import { EntryList } from '../../components/EntryList';
+import { SITE_URL } from '../../constants/site_data';
+import { getEntryList } from '../../entry/entry_gateway';
+import { EntryValue } from '../../entry/entry_value';
+import { retrieveTranslation } from '../../locales/i18n';
+
+type Props = {
+  entries: EntryValue[];
+};
+
+const Entries = (props: Props): JSX.Element => {
+  const { entries } = props;
+  const title = retrieveTranslation('website.title');
+  const description = retrieveTranslation('website.description');
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:title" content={title} />
+        <meta name="description" content={description} />
+        <meta property="og:description" content={description} />
+        <meta property="og:url" content={SITE_URL} />
+        <meta property="og:type" content="website" />
+      </Head>
+      <EntryList title={retrieveTranslation('top.headings.entryList')} entries={entries} />
+    </>
+  );
+};
+
+export async function getStaticProps(): Promise<{ props: Props }> {
+  const entries = await getEntryList();
+
+  return {
+    props: {
+      entries,
+    },
+  };
+}
+
+export default Entries;
