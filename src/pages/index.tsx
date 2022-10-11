dbb9fd930f3e19ea6a65e7f4a787261798401213
Shota Kubota <ta2@o2p.jp>
2022-10-11 22:50:58 +0900

    feat(pages/index): add link to article list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 
 import { SITE_URL } from '../constants/site_data';
 import { EntryList } from '../components/EntryList';
 import { EntryValue } from '../entry/entry_value';
 import { getEntryList } from '../entry/entry_gateway';
 import { retrieveTranslation } from '../locales/i18n';
+
+import styles from './index.module.css';
 
 interface Props {
   entries: EntryValue[];
@@ -26,7 +29,10 @@ const TopPage = (props: Props): JSX.Element => {
         <meta property="og:url" content={SITE_URL} />
         <meta property="og:type" content="website" />
       </Head>
-      <EntryList title={retrieveTranslation('top.headings.entryList')} entries={entries} />
+      <EntryList entries={entries} />
+      <Link href="/entries" as={`/entries`} passHref>
+        <a className={styles['entries-link']}>記事一覧</a>
+      </Link>
     </>
   );
 
@@ -38,7 +44,7 @@ export async function getStaticProps(): Promise<{ props: Props }> {
 
   return {
     props: {
-      entries,
+      entries: entries.slice(0, 10),
     },
   };
 }
