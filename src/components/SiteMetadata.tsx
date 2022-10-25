1bf33a164b4d87218fe75b6f0d116c6f9459fbd9
Shota Kubota <ta2@o2p.jp>
2022-10-25 20:50:46 +0900

    fix(SiteMetadata): move gtm script to outside of Head component

--- src/components/SiteMetadata.tsx
+++ src/components/SiteMetadata.tsx
@@ -15,20 +15,22 @@ export const SiteMetadata = (): JSX.Element => {
   const webSiteTitle = retrieveTranslation('website.title');
 
   return (
-    <Head>
-      <meta name="theme-color" content={MAIN_COLOR} />
-      <meta property="og:site_name" content={webSiteTitle} />
-      <meta property="og:image" content={OG_IMAGE_URL} />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content={OG_IMAGE_URL} />
-      <meta name="twitter:site" content={`@${TWITTER_ACCOUNT_ID}`} />
-      <meta name="Hatena::Bookmark" content="nocomment" />
-      <link rel="apple-touch-icon" href={FAVICON_URL} />
-      <link rel="icon" type="image/png" href={FAVICON_URL} />
-      <link rel="alternate" type="application/atom+xml" href={PathList.Feed} title={webSiteTitle} />
+    <>
+      <Head>
+        <meta name="theme-color" content={MAIN_COLOR} />
+        <meta property="og:site_name" content={webSiteTitle} />
+        <meta property="og:image" content={OG_IMAGE_URL} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={OG_IMAGE_URL} />
+        <meta name="twitter:site" content={`@${TWITTER_ACCOUNT_ID}`} />
+        <meta name="Hatena::Bookmark" content="nocomment" />
+        <link rel="apple-touch-icon" href={FAVICON_URL} />
+        <link rel="icon" type="image/png" href={FAVICON_URL} />
+        <link rel="alternate" type="application/atom+xml" href={PathList.Feed} title={webSiteTitle} />
+      </Head>
       {!gaOptout.enabled() && insertGtmScript(GTM_ID)}
-    </Head>
+    </>
   );
 };
