b6a425d5144ac66f070e4b4a89978842e98fc612
Shota Kubota <ta2@o2p.jp>
2022-12-13 23:08:11 +0900

    test(*): use @kubosho/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,61 +1,12 @@
 module.exports = {
-  root: true,
-  env: { browser: true, node: true, es6: true, jest: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@next/next/recommended',
-    'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'prettier',
-  ],
+  extends: ['@kubosho/eslint-config', 'plugin:@next/next/recommended', 'plugin:@typescript-eslint/recommended'],
   plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
     project: ['./tsconfig.json'],
   },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
-      },
-    },
-    'import/ignore': ['node_modules'],
-    react: {
-      version: 'detect',
-    },
-  },
   rules: {
-    'import/order': [
-      'error',
-      {
-        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-        'newlines-between': 'always',
-      },
-    ],
-    'import/newline-after-import': 'error',
-    'import/no-unresolved': [
-      'error',
-      {
-        caseSensitive: true,
-        commonjs: true,
-      },
-    ],
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: [
-          '**/__tests__/**',
-          '**/__mocks__/**',
-          '**/*.test.js',
-          '**.config.js',
-          '**.config.ts',
-          '**/tools/**',
-        ],
-      },
-    ],
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-use-before-define': ['error', { functions: false, classes: true, variables: true }],
