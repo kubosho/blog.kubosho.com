012b5051d218e5d0d3ff6830b67b2e4410e2a218
Shota Kubota <ta2@o2p.jp>
2023-01-04 17:21:27 +0900

    fix(tracking/gtm): add title in iframe

--- src/tracking/gtm.tsx
+++ src/tracking/gtm.tsx
@@ -20,7 +20,7 @@ export function insertGtmNoscript(id: string): JSX.Element {
   return (
     <noscript>
       <iframe
-        title=""
+        title="Google Tag Manager (noscript)"
         src={`https://www.googletagmanager.com/ns.html?id=${id}`}
         height="0"
         width="0"
