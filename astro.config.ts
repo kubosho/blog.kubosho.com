a3c430f207cee9738b342da69dec9a9f64e14b13
Shota Kubota <ta2@o2p.jp>
2023-02-08 18:47:41 +0900

    chore(prettier): add astro config

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 // eslint-disable-next-line import/no-unresolved
 import react from '@astrojs/react';
+// eslint-disable-next-line import/no-unresolved
+import sitemap from '@astrojs/sitemap';
 
 import { SITE_URL } from './constants/site_data';
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  output: 'server',
+  integrations: [react(), sitemap()],
   site: SITE_URL,
 });
