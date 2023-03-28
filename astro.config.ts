7c7f9dbe1e2bf5a77a2008e44aed77567e7dba92
Shota Kubota <ta2@o2p.jp>
2023-03-28 21:29:41 +0900

    fix(astro.config): add vercel adapter

--- astro.config.ts
+++ astro.config.ts
@@ -3,11 +3,15 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 // eslint-disable-next-line import/no-unresolved
 import sitemap from '@astrojs/sitemap';
+// eslint-disable-next-line import/no-unresolved
+import vercel from '@astrojs/vercel/serverless';
 
 import { SITE_URL } from './constants/site_data';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
   integrations: [react(), sitemap()],
   site: SITE_URL,
+  output: 'server',
 });
