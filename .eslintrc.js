ff5d826b9cc2a437e48855fe934a9e0442b362c0
Shota Kubota <ta2@o2p.jp>
2023-04-04 00:48:51 +0900

    test(eslint): remove unnecessary config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,3 @@
 module.exports = {
-  extends: [require.resolve('@kubosho/configs/eslint'), 'plugin:@typescript-eslint/recommended'],
-  plugins: ['@typescript-eslint'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    sourceType: 'module',
-    project: ['./tsconfig.json'],
-  },
-  rules: {
-    '@typescript-eslint/camelcase': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-use-before-define': ['error', { functions: false, classes: true, variables: true }],
-    '@typescript-eslint/explicit-function-return-type': [
-      'error',
-      {
-        allowExpressions: true,
-        allowTypedFunctionExpressions: true,
-        allowHigherOrderFunctions: true,
-      },
-    ],
-  },
-  overrides: [
-    {
-      files: ['*.js', '*.jsx'],
-      rules: {
-        '@typescript-eslint/no-var-requires': 'off',
-      },
-    },
-  ],
+  extends: [require.resolve('@kubosho/configs/eslint')],
 };
