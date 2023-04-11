22e67dd0329adb246540d475b27a662f0dce2404
Shota Kubota <ta2@o2p.jp>
2023-04-12 01:02:46 +0900

    fix(*): fix GA4 not working

--- src/stores/analyticsOptoutStore.ts
+++ src/stores/analyticsOptoutStore.ts
@@ -1,9 +1,10 @@
 import { atom } from 'nanostores';
 
 import { createGAOptout } from '../tracking/ga_optout';
-import { GTM_ID } from '../tracking/gtm_id';
+import { GTM_ID } from '../tracking/gtm_data';
 
 export const analyticsOptout = createGAOptout(GTM_ID);
+
 export const analyticsOptoutState = atom(false);
 
 export const setAnalyticsOptoutState = (value: boolean): void => {
