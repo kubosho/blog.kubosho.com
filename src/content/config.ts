b9271bff065e578294c8f510a494f79431f20297
Shota Kubota <ta2@o2p.jp>
2024-06-18 00:55:55 +0900

    feat(src): add Astro content collection config file

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from 'astro:content';
+
+const entries = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    excerpt: z.string().optional(),
+    publishedAt: z.date(),
+    revisedAt: z.date().optional(),
+    categories: z.array(z.string()).optional(),
+    tags: z.array(z.string()).optional(),
+    heroImageUrl: z.string().optional(),
+    ogImageUrl: z.string().optional(),
+  }),
+});
+
+export const collections = { entries };
