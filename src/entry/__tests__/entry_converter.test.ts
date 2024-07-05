c66784a5bd59713b7271cb990b196b54529f85d3
Shota Kubota <ta2@o2p.jp>
2024-07-06 00:07:29 +0900

    test(entry): wrap tests in describe block

--- src/entry/__tests__/entry_converter.test.ts
+++ src/entry/__tests__/entry_converter.test.ts
@@ -5,30 +5,32 @@ import { describe, expect, test } from 'vitest';
 
 import { convertMarkdownToHtml, convertMarkdownToPlainText } from '../entry_converter';
 
-describe('convertMarkdownToHtml', () => {
-  test('Markdown text is converted to intended HTML', async () => {
-    // Given
-    const markdown = await readFile(path.resolve(__dirname, '../fixtures/sauna.md'), 'utf-8');
-    const expected = await readFile(path.resolve(__dirname, '../fixtures/sauna.html'), 'utf-8');
+describe('EntryConverter', () => {
+  describe('convertMarkdownToHtml', () => {
+    test('Markdown text is converted to intended HTML', async () => {
+      // Given
+      const markdown = await readFile(path.resolve(__dirname, '../fixtures/sauna.md'), 'utf-8');
+      const expected = await readFile(path.resolve(__dirname, '../fixtures/sauna.html'), 'utf-8');
 
-    // When
-    const htmlString = await convertMarkdownToHtml(markdown);
+      // When
+      const htmlString = await convertMarkdownToHtml(markdown);
 
-    // Then
-    expect(htmlString).toBe(expected);
+      // Then
+      expect(htmlString).toBe(expected);
+    });
   });
-});
 
-describe('convertMarkdownToPlainText', () => {
-  test('Markdown text is converted to intended plain text', async () => {
-    // Given
-    const markdown = await readFile(path.resolve(__dirname, '../fixtures/sauna.md'), 'utf-8');
-    const expected = await readFile(path.resolve(__dirname, '../fixtures/sauna.txt'), 'utf-8');
+  describe('convertMarkdownToPlainText', () => {
+    test('Markdown text is converted to intended plain text', async () => {
+      // Given
+      const markdown = await readFile(path.resolve(__dirname, '../fixtures/sauna.md'), 'utf-8');
+      const expected = await readFile(path.resolve(__dirname, '../fixtures/sauna.txt'), 'utf-8');
 
-    // When
-    const plainText = await convertMarkdownToPlainText(markdown);
+      // When
+      const plainText = await convertMarkdownToPlainText(markdown);
 
-    // Then
-    expect(plainText).toBe(expected);
+      // Then
+      expect(plainText).toBe(expected);
+    });
   });
 });
