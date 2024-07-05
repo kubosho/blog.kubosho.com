7cb0ea7d2830d12207ca1296df15ad0941eea6a1
Shota Kubota <ta2@o2p.jp>
2024-07-05 11:04:27 +0900

    feat(entry): implement Markdown to HTML converter

--- src/entry/entry_converter.ts
+++ src/entry/entry_converter.ts
@@ -1,40 +1,19 @@
-import { Processor, unified } from 'unified';
+import rehypePrism from '@mapbox/rehype-prism';
+import html from 'rehype-stringify';
+import breaks from 'remark-breaks';
 import gfm from 'remark-gfm';
 import markdown from 'remark-parse';
-import stringify from 'remark-stringify';
-import breaks from 'remark-breaks';
-import strip from 'strip-markdown';
 import remarkToRehype from 'remark-rehype';
-import html from 'rehype-stringify';
-import rehypePrism from '@mapbox/rehype-prism';
+import { unified } from 'unified';
 
-import type { BlogApiSchema } from '../microcms_api/api_schema';
+const markdownProcessor = unified().use(markdown).use(gfm);
 
-import { EntryValue } from './entry_value';
-
-export async function mapEntryValue(contents: BlogApiSchema): Promise<EntryValue> {
-  const { title, body: originalBody, excerpt: originalExcerpt, categories, tags } = contents;
-
-  const markdownProcessor = (): Processor => unified().use(markdown).use(gfm);
-  const contentsProcessor = markdownProcessor()
+export async function convertMarkdownToHtml(markdownText: string): Promise<string> {
+  const processor = markdownProcessor()
     .use(breaks)
     .use(remarkToRehype, { allowDangerousHtml: true })
     .use(rehypePrism, { ignoreMissing: true })
     .use(html, { allowDangerousHtml: true });
-  const excerptProcessor = markdownProcessor().use(strip).use(stringify);
 
-  const body = await contentsProcessor.process(originalBody);
-  const excerpt = await excerptProcessor.process(originalBody.split('\n')[0].replace(/\\$/, ''));
-
-  const categoryList = categories?.map((category) => category.trim()) ?? [];
-  const tagList = tags?.map((tag) => tag.trim()) ?? [];
-
-  return new EntryValue({
-    ...contents,
-    title,
-    body: body.value.toString(),
-    excerpt: originalExcerpt ?? excerpt.value.toString().trim(),
-    categories: categoryList,
-    tags: tagList,
-  });
+  return (await processor.process(markdownText)).value.toString();
 }
