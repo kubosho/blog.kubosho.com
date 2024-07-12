81a1b7c51fb8bbe647fa156f1ebd38dbc6a12836
Shota Kubota <ta2@o2p.jp>
2024-07-13 01:03:26 +0900

    chore(eslint): ignore .astro directory

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,6 @@ import config from '@kubosho/configs/eslint/index.mjs';
 export default [
   ...config,
   {
-    files: ['**/*.ts', '**/*.tsx'],
-  },
-  {
-    ignores: ['**/.astro/types.d.ts'],
+    ignores: ['.astro/'],
   },
 ];
