c4f659c42a366318897e21a956a5448c87cf8627
Shota Kubota <ta2@o2p.jp>
2024-07-14 13:14:10 +0900

    build(astro)!: set hybrid mode

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
 
 import { SITE_URL } from './constants/site_data';
 
-// https://astro.build/config
 export default defineConfig({
+  adapter: cloudflare(),
   integrations: [react(), sitemap()],
+  output: 'hybrid',
   site: SITE_URL,
 });
