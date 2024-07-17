2950a7933cc3ffb864365b1bfda2be74c8748346
Shota Kubota <ta2@o2p.jp>
2024-07-17 13:58:22 +0900

    build(*): use @astro/node adapter when USE_NODE_ADAPTER=true

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,22 @@
 import cloudflare from '@astrojs/cloudflare';
+import node from '@astrojs/node';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import type { AstroIntegration } from 'astro';
 import { defineConfig } from 'astro/config';
 
 import { SITE_URL } from './constants/site_data';
 
+function getAdapter(): AstroIntegration {
+  if (process.env.USE_NODE_ADAPTER) {
+    return node({ mode: 'standalone' });
+  }
+
+  return cloudflare();
+}
+
 export default defineConfig({
-  adapter: cloudflare(),
+  adapter: getAdapter(),
   integrations: [react(), sitemap()],
   output: 'hybrid',
   site: SITE_URL,
