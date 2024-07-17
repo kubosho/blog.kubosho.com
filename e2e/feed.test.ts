d67bf711025ffe8b2c9e23842d19c2883aea2b7b
Shota Kubota <ta2@o2p.jp>
2024-07-17 14:37:29 +0900

    refactor(feed): split feed page into UA and CI versions

--- e2e/feed.test.ts
+++ e2e/feed.test.ts
@@ -5,7 +5,7 @@ import { LOCAL_SITE_URL } from '../constants/site_data';
 test.describe('Feed', () => {
   test('Should be can access to feed', async ({ page }) => {
     // When
-    const response = await page.goto(`${LOCAL_SITE_URL}/feed`);
+    const response = await page.goto(`${LOCAL_SITE_URL}/feed.xml`);
 
     // Then
     expect(response?.status()).toBe(200);
@@ -13,7 +13,7 @@ test.describe('Feed', () => {
 
   test('Should be the Content-Type is application/xml', async ({ page }) => {
     // When
-    const response = await page.goto(`${LOCAL_SITE_URL}/feed`);
+    const response = await page.goto(`${LOCAL_SITE_URL}/feed.xml`);
 
     // Then
     expect(await response?.headerValue('content-type')).toBe('application/xml');
