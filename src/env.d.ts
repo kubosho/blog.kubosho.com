c1b88c0192cecfd3361b4103d8373b06dee370c2
Shota Kubota <ta2@o2p.jp>
2024-07-20 01:19:14 +0900

    chore(*): remove unused environment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,16 +2,6 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
-interface ImportMetaEnv {
-  readonly X_MICROCMS_API_SUB_DOMAIN: string;
-  readonly X_MICROCMS_API_NAME: string;
-  readonly X_MICROCMS_API_KEY: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
-
 declare module 'process' {
   global {
     namespace NodeJS {
