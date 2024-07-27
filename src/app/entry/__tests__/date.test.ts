5eb820d55bb6363951ef7a6a9f43d3fd7b1ee23c
Shota Kubota <ta2@o2p.jp>
2024-07-27 15:45:44 +0900

    chore(date): wrap date tests in describe() block

--- src/app/entry/__tests__/date.test.ts
+++ src/app/entry/__tests__/date.test.ts
@@ -1,52 +1,54 @@
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 
 import { convertISOStringToMilliseconds, formatISOString, formatRFC2822, formatYYMMDDString } from '../date';
 
-test('convertISOStringToMilliseconds()', async () => {
-  const isoString = '2011-10-05T14:48:00.000Z';
+describe('date', () => {
+  test('convertISOStringToMilliseconds()', async () => {
+    const isoString = '2011-10-05T14:48:00.000Z';
 
-  const actualValue = convertISOStringToMilliseconds(isoString);
-  const expectValue = 1317826080000;
+    const actualValue = convertISOStringToMilliseconds(isoString);
+    const expectValue = 1317826080000;
 
-  expect(actualValue).toBe(expectValue);
-});
+    expect(actualValue).toBe(expectValue);
+  });
 
-test('formatISOString()', async () => {
-  // '2011-10-05T14:48:00.000Z'
-  const milliseconds = 1317826080000;
+  test('formatISOString()', async () => {
+    // '2011-10-05T14:48:00.000Z'
+    const milliseconds = 1317826080000;
 
-  const actualValue = formatISOString(milliseconds);
-  const expectValue = '2011-10-05T14:48:00.000Z';
+    const actualValue = formatISOString(milliseconds);
+    const expectValue = '2011-10-05T14:48:00.000Z';
 
-  expect(actualValue).toBe(expectValue);
-});
+    expect(actualValue).toBe(expectValue);
+  });
 
-test('formatRFC2822()', async () => {
-  // '2011-10-05T14:48:00.000Z'
-  const milliseconds = 1317826080000;
+  test('formatRFC2822()', async () => {
+    // '2011-10-05T14:48:00.000Z'
+    const milliseconds = 1317826080000;
 
-  const actualValue = formatRFC2822(milliseconds);
-  const expectValue = 'Wed, 05 Oct 2011 14:48:00 +0000';
+    const actualValue = formatRFC2822(milliseconds);
+    const expectValue = 'Wed, 05 Oct 2011 14:48:00 +0000';
 
-  expect(actualValue).toBe(expectValue);
-});
+    expect(actualValue).toBe(expectValue);
+  });
 
-test('formatYYMMDDString()', async () => {
-  // '2011-10-05T14:48:00.000Z'
-  const milliseconds = 1317826080000;
+  test('formatYYMMDDString()', async () => {
+    // '2011-10-05T14:48:00.000Z'
+    const milliseconds = 1317826080000;
 
-  const actualValue = formatYYMMDDString(milliseconds);
-  const expectValue = '2011.10.05';
+    const actualValue = formatYYMMDDString(milliseconds);
+    const expectValue = '2011.10.05';
 
-  expect(actualValue).toBe(expectValue);
-});
+    expect(actualValue).toBe(expectValue);
+  });
 
-test('formatYYMMDDString(): set separator argument', async () => {
-  // '2011-10-05T14:48:00.000Z'
-  const milliseconds = 1317826080000;
+  test('formatYYMMDDString(): set separator argument', async () => {
+    // '2011-10-05T14:48:00.000Z'
+    const milliseconds = 1317826080000;
 
-  const actualValue = formatYYMMDDString(milliseconds, '-');
-  const expectValue = '2011-10-05';
+    const actualValue = formatYYMMDDString(milliseconds, '-');
+    const expectValue = '2011-10-05';
 
-  expect(actualValue).toBe(expectValue);
+    expect(actualValue).toBe(expectValue);
+  });
 });
