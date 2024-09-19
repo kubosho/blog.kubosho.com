835823b2cf5816957ccdadb54f8232fff555902d
Shota Kubota <ta2@o2p.jp>
2024-09-19 11:26:10 +0900

    feat(*): replace to new og:image on each entry page

--- constants/site_data.ts
+++ constants/site_data.ts
@@ -1,7 +1,6 @@
-export const BASE_LANGUAGE = 'ja';
+export const BASE_LANGUAGE = 'ja' as const;
 
-export const AUTHOR = 'kubosho';
-export const AUTHOR_WITH_UNDERSCORE = `${AUTHOR}_`;
+export const AUTHOR = 'kubosho' as const;
 
 export const SITE_HOSTNAME = 'blog.kubosho.com' as const;
 export const SITE_URL = `https://${SITE_HOSTNAME}` as const;
@@ -9,8 +8,11 @@ export const LOCAL_SITE_HOSTNAME = 'localhost' as const;
 export const LOCAL_SITE_PORT = 4321 as const;
 export const LOCAL_SITE_URL = `http://${LOCAL_SITE_HOSTNAME}:${LOCAL_SITE_PORT}` as const;
 
-export const TWITTER_ACCOUNT_ID = AUTHOR_WITH_UNDERSCORE;
-export const FACEBOOK_APP_ID = '2453282784920956';
+export const TWITTER_ACCOUNT_ID = `${AUTHOR}_` as const;
+export const FACEBOOK_APP_ID = '2453282784920956' as const;
 
-export const FAVICON_URL = 'https://blog-assets.kubosho.com/icon.png';
-export const OG_IMAGE_URL = 'https://blog-assets.kubosho.com/og_image.png';
+export const ASSETS_URL = 'https://blog-assets.kubosho.com' as const;
+export const FAVICON_URL = `${ASSETS_URL}/icon.png` as const;
+
+export const OG_IMAGE_PATH = `${ASSETS_URL}/images/og` as const;
+export const FALLBACK_OG_IMAGE_URL = `${OG_IMAGE_PATH}/fallback_og_image.png` as const;
