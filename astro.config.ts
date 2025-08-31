19e93226c234ca92ae6f666057d4fe250d8d3c78
KUBOTA Shota <ta2+github@o2p.jp>
2025-08-31 23:45:00 +0900

    build(astro): remove SITE_URL definition

--- astro.config.ts
+++ astro.config.ts
@@ -5,8 +5,6 @@ import sitemap from '@astrojs/sitemap';
 import type { AstroIntegration } from 'astro';
 import { defineConfig } from 'astro/config';
 
-import { SITE_URL } from './constants/site_data';
-
 function getAdapter(): AstroIntegration {
   if (process.env.USE_NODE_ADAPTER) {
     return node({ mode: 'standalone' });
@@ -25,7 +23,6 @@ export default defineConfig({
     syntaxHighlight: 'prism',
   },
   output: 'static',
-  site: SITE_URL,
   vite: {
     define: {
       PUBLIC_API_BASE_URL: JSON.stringify(import.meta.env.PUBLIC_API_BASE_URL ?? ''),
