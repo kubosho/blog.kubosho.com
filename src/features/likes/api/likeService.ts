17df0a926d192c3550d7d6fd8542f7ae8d242e54
KUBOTA Shota <ta2+github@o2p.jp>
2025-09-01 19:14:48 +0900

    feat(likes): implement likes functionality

---
+++ src/features/likes/api/likeService.ts
@@ -0,0 +1,40 @@
+import { eq, sum } from 'drizzle-orm';
+import { drizzle, type PostgresJsDatabase } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+
+import { likes } from './databaseSchema';
+
+export interface LikeServiceInterface {
+  getLikeCount(entryId: string): Promise<number>;
+  addLikes(entryId: string, count: number): Promise<number>;
+}
+
+export class LikeService implements LikeServiceInterface {
+  private _db: PostgresJsDatabase;
+
+  constructor(databaseUrl: string) {
+    const client = postgres(databaseUrl);
+
+    this._db = drizzle(client);
+  }
+
+  async getLikeCount(entryId: string): Promise<number> {
+    const result = await this._db
+      .select({ total: sum(likes.counts) })
+      .from(likes)
+      .where(eq(likes.entryId, entryId));
+
+    const total = Number(result[0]?.total ?? 0);
+
+    return total;
+  }
+
+  async addLikes(entryId: string, count: number): Promise<number> {
+    await this._db.insert(likes).values({
+      entryId,
+      counts: count,
+    });
+
+    return this.getLikeCount(entryId);
+  }
+}
