db901fe7029d8e7a094b1e6a93051d9e23ad711b
KUBOTA Shota <ta2+github@o2p.jp>
2025-09-15 14:18:55 +0900

    fix(stylelint): add rule for unknown declaration property values with rgb()
    Related links:
    
    - https://github.com/stylelint/stylelint/issues/8347

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -3,4 +3,13 @@ import config from '@kubosho/configs/stylelint';
 export default {
   ...config,
   ignoreFiles: ['dist/**/*.css', 'node_modules/**/*.css'],
+
+  rules: {
+    'declaration-property-value-no-unknown': [
+      true,
+      {
+        typesSyntax: { 'rgb()': '| <any-value>' },
+      },
+    ],
+  },
 };
