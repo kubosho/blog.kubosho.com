77528d0e1e60029e7feef5a59384ba0ab147bb7b
KUBOTA Shota <ta2+github@o2p.jp>
2025-12-12 12:48:34 +0900

    feat: implement LikeCountsSkeleton components

---
+++ src/features/likes/components/LikeCountsSkeleton/LikeCountsSkeleton.tsx
@@ -0,0 +1,5 @@
+import styles from './LikeCountsSkeleton.module.css';
+
+export function LikeCountsSkeleton(): React.JSX.Element {
+  return <span className={styles.skeleton} />;
+}
---
+++ src/features/likes/components/LikeCountsSkeleton/index.ts
@@ -0,0 +1,1 @@
+export { LikeCountsSkeleton } from './LikeCountsSkeleton';
