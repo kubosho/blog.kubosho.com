7e9bcfcf36d1e115a131eba552c0f8e24eac7438
KUBOTA Shota <ta2+github@o2p.jp>
2025-12-13 22:43:30 +0900

    fix: disable cache to prevent query errors on Cloudflare Workers

--- src/features/likes/api/database.ts
+++ src/features/likes/api/database.ts
@@ -3,23 +3,31 @@ import postgres from 'postgres';
 
 const dbMap = new Map<string, PostgresJsDatabase>();
 
+const isCloudflareWorkersRuntime = typeof navigator !== 'undefined' && navigator.userAgent === 'Cloudflare-Workers';
+
 export function createDatabaseClient(databaseUrl: string | null | undefined): PostgresJsDatabase {
   if (databaseUrl == null || databaseUrl === '') {
     throw new Error('DATABASE_URL is not defined');
   }
 
-  const cachedDB = dbMap.get(databaseUrl);
-  if (cachedDB != null) {
-    return cachedDB;
+  if (!isCloudflareWorkersRuntime) {
+    const cachedDB = dbMap.get(databaseUrl);
+    if (cachedDB != null) {
+      return cachedDB;
+    }
   }
 
   const client = postgres(databaseUrl, {
     connect_timeout: 5,
     idle_timeout: 10,
+    max: 1,
+    prepare: false,
   });
   const db = drizzle(client);
 
-  dbMap.set(databaseUrl, db);
+  if (!isCloudflareWorkersRuntime) {
+    dbMap.set(databaseUrl, db);
+  }
 
   return db;
 }
