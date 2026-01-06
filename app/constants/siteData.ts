9d3c1bd71ce6725bebe0a73e9c173488896e896c
KUBOTA Shota <ta2+github@o2p.jp>
2026-01-06 17:35:26 +0900

    feat: apply new favicons

--- app/constants/siteData.ts
+++ app/constants/siteData.ts
@@ -9,7 +9,6 @@ export const TWITTER_ACCOUNT_ID = `${AUTHOR}_` as const;
 export const FACEBOOK_APP_ID = '2453282784920956' as const;
 
 export const ASSETS_URL = 'https://blog-assets.kubosho.com' as const;
-export const FAVICON_URL = `${ASSETS_URL}/icon.png` as const;
 
 export const OG_IMAGE_PATH = `${ASSETS_URL}/images/og` as const;
 export const FALLBACK_OG_IMAGE_URL = `${OG_IMAGE_PATH}/fallback_og_image.png` as const;
